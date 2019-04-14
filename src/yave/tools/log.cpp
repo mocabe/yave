86c3c34830edc492ca47aee004ec388f33cc8ac7
mocabe <hisa.mocabe@gmail.com>
2019-04-14 21:14:05 +0900

    tools: Added log.cpp

---
+++ src/yave/tools/log.cpp
@@ -0,0 +1,22 @@
+//
+// Copyright (c) 2019 mocabe (https://github.com/mocabe)
+// Distributed under LGPLv3 License. See LICENSE for more details.
+//
+
+#include <yave/tools/log.hpp>
+
+namespace yave {
+
+  std::shared_ptr<spdlog::logger> g_logger = spdlog::stdout_color_mt("yave");
+
+  std::shared_ptr<spdlog::logger> add_logger(const char* name)
+  {
+    return spdlog::stderr_color_mt(name);
+  }
+
+  std::shared_ptr<spdlog::logger> get_logger(const char* name)
+  {
+    return spdlog::get(name);
+  }
+
+}
