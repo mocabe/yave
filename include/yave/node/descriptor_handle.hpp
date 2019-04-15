81a3200f1856614c5160cc87854bc283a92813e2
mocabe <hisa.mocabe@gmail.com>
2019-04-15 20:17:32 +0900

    node: Added descriptor handle

---
+++ include/yave/node/descriptor_handle.hpp
@@ -0,0 +1,92 @@
+//
+// Copyright (c) 2019 mocabe (https://github.com/mocabe)
+// Distributed under LGPLv3 License. See LICENSE for more details.
+//
+
+#pragma once
+
+#include <yave/config/config.hpp>
+
+namespace yave {
+
+  /// Simple wrapper for descriptor pointers.
+  template <class Descriptor>
+  class DescriptorHandle
+  {
+  public:
+    /// Constructor
+    constexpr DescriptorHandle(std::nullptr_t) noexcept
+      : m_descriptor {nullptr}
+    {
+    }
+    /// Constructor
+    constexpr DescriptorHandle(const Descriptor& d) noexcept
+      : m_descriptor {d}
+    {
+    }
+    /// Copy Constructor
+    constexpr DescriptorHandle(const DescriptorHandle&) = default;
+    /// Move Constructor
+    constexpr DescriptorHandle(DescriptorHandle&&) = default;
+    /// operator=
+    constexpr DescriptorHandle& operator=(const DescriptorHandle&) = default;
+    /// operator=
+    constexpr DescriptorHandle& operator=(DescriptorHandle&&) = default;
+
+    /// check if having valid value
+    constexpr bool_t has_value() const noexcept
+    {
+      return static_cast<bool>(m_descriptor);
+    }
+
+    /// check if having valid value
+    constexpr explicit operator bool() const noexcept
+    {
+      return has_value();
+    }
+
+    /// Get descriptor value.
+    constexpr Descriptor descriptor() const noexcept
+    {
+      return m_descriptor;
+    }
+
+    /// Get descriptor id.
+    constexpr std::uintptr_t id() const noexcept
+    {
+      static_assert(sizeof(Descriptor) <= sizeof(uintptr_t));
+      return reinterpret_cast<std::uintptr_t>(m_descriptor);
+    }
+
+  private:
+    /// discriptor pointer
+    Descriptor m_descriptor;
+  };
+
+  /// operator==
+  template <class D>
+  constexpr bool operator==(
+    const DescriptorHandle<D>& lhs,
+    const DescriptorHandle<D>& rhs) noexcept
+  {
+    return lhs.descriptor() == rhs.descriptor();
+  }
+
+  /// operator!=
+  template <class D>
+  constexpr bool operator!=(
+    const DescriptorHandle<D>& lhs,
+    const DescriptorHandle<D>& rhs) noexcept
+  {
+    return lhs.descriptor() != rhs.descriptor();
+  }
+
+  template <class D>
+  constexpr bool operator<(
+    const DescriptorHandle<D>& lhs,
+    const DescriptorHandle<D>& rhs) noexcept
+  {
+    return lhs.descriptor() < rhs.descriptor();
+  }
+
+} // namespace yave
