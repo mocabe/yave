d539ec607eb6d021441c0d4b7bd76d383a34e56d
mocabe <hisa.mocabe@gmail.com>
2019-04-15 20:18:13 +0900

    node: Added primitive and graph definitions

---
+++ include/yave/node/graph_definition.hpp
@@ -0,0 +1,17 @@
+//
+// Copyright (c) 2019 mocabe (https://github.com/mocabe)
+// Distributed under LGPLv3 License. See LICENSE for more details.
+//
+
+#pragma once
+
+#include <yave/config/config.hpp>
+#include <yave/graph/graph.hpp>
+#include <yave/node/node_property.hpp>
+#include <yave/node/socket_property.hpp>
+#include <yave/node/edge_property.hpp>
+
+namespace yave {
+  /// Graph type used in node tree
+  using graph_t = Graph<NodeProperty, SocketProperty, EdgeProperty>;
+}
