22dd8a57464c0d53d8e9d4f422f78d09fd3a3fec
mocabe <hisa.mocabe@gmail.com>
2019-04-16 01:55:11 +0900

    Added some detail namespace around impl

--- include/yave/config/type_traits.hpp
+++ include/yave/config/type_traits.hpp
@@ -20,39 +20,47 @@ namespace yave {
   // ------------------------------------------
   // is_complete
 
-  template <class T>
-  constexpr char is_complete_impl_func(int (*)[sizeof(T)]);
+  namespace detail {
 
-  template <class>
-  constexpr long is_complete_impl_func(...);
+    template <class T>
+    constexpr char is_complete_impl_func(int (*)[sizeof(T)]);
+
+    template <class>
+    constexpr long is_complete_impl_func(...);
+
+    template <class T>
+    struct is_complete_impl
+    {
+      static constexpr bool value = sizeof(is_complete_impl_func<T>(0)) == 1;
+    };
+
+  } // namespace detail
 
   template <class T>
-  struct is_complete_impl
-  {
-    static constexpr bool value = sizeof(is_complete_impl_func<T>(0)) == 1;
-  };
-
-  template <class T>
-  constexpr bool is_complete_v = is_complete_impl<T>::value;
+  constexpr bool is_complete_v = detail::is_complete_impl<T>::value;
 
   // ------------------------------------------
   // propagate_const
 
-  template <class T, class U>
-  struct propagate_const_impl
-  {
-    using type = T;
-  };
+  namespace detail {
 
-  template <class T, class U>
-  struct propagate_const_impl<T, const U>
-  {
-    using type = std::add_const_t<T>;
-  };
+    template <class T, class U>
+    struct propagate_const_impl
+    {
+      using type = T;
+    };
+
+    template <class T, class U>
+    struct propagate_const_impl<T, const U>
+    {
+      using type = std::add_const_t<T>;
+    };
+
+  } // namespace detail
 
   /// conditionally add const to T depending on constness of U
   template <class T, class U>
-  using propagate_const_t = typename propagate_const_impl<T, U>::type;
+  using propagate_const_t = typename detail::propagate_const_impl<T, U>::type;
 
   // ------------------------------------------
   // concept_checker
