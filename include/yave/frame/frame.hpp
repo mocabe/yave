f0662d3448966d5f4fe2e27830e0c8f1d2926513
mocabe <hisa.mocabe@gmail.com>
2019-04-17 13:26:49 +0900

    frame: Added frame data type

---
+++ include/yave/frame/frame.hpp
@@ -0,0 +1,19 @@
+//
+// Copyright (c) 2019 mocabe (https://github.com/mocabe)
+// Distributed under LGPLv3 License. See LICENSE for more details.
+//
+
+#pragma once
+
+#include <yave/time/time.hpp>
+
+namespace yave {
+
+  /// Data type to represent single frame point.
+  struct frame
+  {
+    /// Time point of frame
+    time time;
+  };
+
+} // namespace yave
