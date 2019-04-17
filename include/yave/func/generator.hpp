9c45bed13de445223aa130facf52f21272c4d146
mocabe <hisa.mocabe@gmail.com>
2019-04-17 20:43:38 +0900

    func: Added generators

---
+++ include/yave/func/generator.hpp
@@ -0,0 +1,95 @@
+//
+// Copyright (c) 2019 mocabe (https://github.com/mocabe)
+// Distributed under LGPLv3 License. See LICENSE for more details.
+//
+
+#pragma once
+
+#include <yave/rts/function.hpp>
+#include <yave/rts/eval.hpp>
+#include <yave/obj/frame.hpp>
+#include <functional>
+
+namespace yave {
+
+  /// UnaryFunction
+  template <class T1, class TR, class E>
+  struct UnaryFunction //
+    : Function<
+        UnaryFunction<T1, TR, E>,
+        closure<Frame::Frame, T1>,
+        Frame::Frame,
+        TR>
+  {
+    typename UnaryFunction::return_type code() const
+    {
+      auto v0 = eval(this->template arg<0>() << this->template arg<1>());
+      return make_object<TR>(e(*v0));
+      E e;
+    }
+  };
+
+  /// BinaryFunction
+  template <class T1, class T2, class TR, class E>
+  struct BinaryFunction //
+    : Function<
+        BinaryFunction<T1, T2, TR, E>,
+        closure<Frame::Frame, T1>,
+        closure<Frame::Frame, T2>,
+        Frame::Frame,
+        TR>
+  {
+    typename BinaryFunction::return_type code() const
+    {
+      auto v0 = eval(this->template arg<0>() << this->template arg<2>());
+      auto v1 = eval(this->template arg<1>() << this->template arg<2>());
+      E e;
+      return make_object<TR>(e(*v0, *v1));
+    }
+  };
+
+  /// TernaryFunction
+  template <class T1, class T2, class T3, class TR, class E>
+  struct TernaryFunction //
+    : Function<
+        TernaryFunction<T1, T2, T3, TR, E>,
+        closure<Frame::Frame, T1>,
+        closure<Frame::Frame, T2>,
+        closure<Frame::Frame, T3>,
+        Frame::Frame,
+        TR>
+  {
+    typename TernaryFunction::return_type code() const
+    {
+      auto v0 = eval(this->template arg<0>() << this->template arg<3>());
+      auto v1 = eval(this->template arg<1>() << this->template arg<3>());
+      auto v2 = eval(this->template arg<2>() << this->template arg<3>());
+      E e;
+      return make_object<TR>(e(*v0, *v1, *v2));
+    }
+  };
+
+  /// QuaternaryFunction
+  template <class T1, class T2, class T3, class T4, class TR, class E>
+  struct QuaternaryFunction //
+    : Function<
+        QuaternaryFunction<T1, T2, T3, T4, TR, E>,
+        closure<Frame::Frame, T1>,
+        closure<Frame::Frame, T2>,
+        closure<Frame::Frame, T3>,
+        closure<Frame::Frame, T4>,
+        Frame::Frame,
+        TR>
+  {
+    typename QuaternaryFunction::return_type code() const
+    {
+      auto v0 = eval(this->template arg<0>() << this->template arg<4>());
+      auto v1 = eval(this->template arg<1>() << this->template arg<4>());
+      auto v2 = eval(this->template arg<2>() << this->template arg<4>());
+      auto v3 = eval(this->template arg<3>() << this->template arg<4>());
+      E e;
+      return make_object<TR>(e(*v0, *v1, *v2, *v3));
+    }
+  };
+
+} // namespace yave
---
+++ test/func/generator.cpp
@@ -0,0 +1,77 @@
+//
+// Copyright (c) 2019 mocabe (https://github.com/mocabe)
+// Distributed under LGPLv3 License. See LICENSE for more details.
+//
+
+#include <yave/func/generator.hpp>
+#include <catch2/catch.hpp>
+#include <yave/rts/string.hpp>
+
+using namespace yave;
+
+using Int    = Box<int>;
+using Float  = Box<float>;
+using Double = Box<double>;
+YAVE_DECL_TYPE(Int, Int);
+YAVE_DECL_TYPE(Float, Float);
+YAVE_DECL_TYPE(Double, Double);
+
+TEST_CASE("generators")
+{
+  SECTION("Unary")
+  {
+    {
+      struct func
+      {
+        int operator()(int)
+        {
+          return 42;
+        }
+      };
+      using Func = UnaryFunction<Int, Int, func>;
+      auto v     = make_object<Func>();
+    }
+  }
+  SECTION("Binary")
+  {
+    {
+      struct func
+      {
+        float operator()(int, float)
+        {
+          return 42;
+        }
+      };
+      using Func = BinaryFunction<Int, Float, Float, func>;
+      auto v     = make_object<Func>();
+    }
+  }
+  SECTION("Ternary")
+  {
+    {
+      struct func
+      {
+        float operator()(int, float, double)
+        {
+          return 42;
+        }
+      };
+      using Max = TernaryFunction<Int, Float, Double, Float, func>;
+      auto v    = make_object<Max>();
+    }
+  }
+  SECTION("Quaternary")
+  {
+    {
+      struct func
+      {
+        float operator()(int, float, double, int)
+        {
+          return 42;
+        }
+      };
+      using Func = QuaternaryFunction<Int, Float, Double, Int, Float, func>;
+      auto v     = make_object<Func>;
+    }
+  }
+}
