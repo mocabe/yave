7419ae881dde7b4e6acc95ff4314e8fdc680dc2b
mocabe <hisa.mocabe@gmail.com>
2019-04-18 16:15:32 +0900

    node: Added ConnectionHandle

---
+++ include/yave/node/connection_handle.hpp
@@ -0,0 +1,14 @@
+//
+// Copyright (c) 2019 mocabe (https://github.com/mocabe)
+// Distributed under LGPLv3 License. See LICENSE for more details.
+//
+
+#pragma once
+
+#include <yave/node/graph_definition.hpp>
+#include <yave/node/descriptor_handle.hpp>
+
+namespace yave {
+  /// Handle of socket connection
+  using ConnectionHandle = DescriptorHandle<graph_t::edge_descriptor_type>;
+} // namespace yave
