66919b1e5e6528f8625d69306481ba14c744644d
mocabe <hisa.mocabe@gmail.com>
2019-04-18 11:04:23 +0900

    rts: Added generalize functions

---
+++ include/yave/rts/generalize.hpp
@@ -0,0 +1,22 @@
+//
+// Copyright (c) 2019 mocabe (https://github.com/mocabe)
+// Distributed under LGPLv3 License. See LICENSE for more details.
+//
+
+#pragma once
+
+#include <yave/rts/dynamic_typing.hpp>
+
+namespace yave {
+
+  /// Anti-unification.
+  /// \param ts Not-empty list of types
+  object_ptr<const Type>
+    generalize(const std::vector<object_ptr<const Type>>& ts);
+
+  /// specializable
+  bool specializable(
+    const object_ptr<const Type>& t1,
+    const object_ptr<const Type>& t2);
+
+} // namespace yave
