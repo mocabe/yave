2828d4b464986aa0a5b4a996d05cc7cc636c97ea
mocabe <hisa.mocabe@gmail.com>
2019-04-28 00:52:57 +0900

    node: BindInfoManager now uses std::shared_ptr

--- include/yave/node/bind_info_manager.hpp
+++ include/yave/node/bind_info_manager.hpp
@@ -10,21 +10,20 @@
 #include <functional>
 #include <mutex>
 #include <map>
+#include <memory>
 
 namespace yave {
 
   /// Manager of BindInfo instances.
   class BindInfoManager
   {
+    /// map type
+    using map_type =
+      std::multimap<std::string, std::shared_ptr<const BindInfo>>;
+
   public:
     /// info type
     using info_type = BindInfo;
-    /// map type
-    using map_type = std::multimap<std::string, info_type>;
-    /// iterator type
-    using iterator = typename map_type::iterator;
-    /// const iterator type
-    using const_iterator = typename map_type::const_iterator;
 
     /// Copy constructro
     BindInfoManager(const BindInfoManager& other);
@@ -38,6 +37,9 @@ namespace yave {
     /// Constructor
     BindInfoManager();
 
+    /// Check if the info exists
+    [[nodiscard]] bool exists(const std::string& name) const;
+
     /// Add info
     [[nodiscard]] bool add(const BindInfo& info);
 
@@ -45,35 +47,29 @@ namespace yave {
     void remove(const std::string& name);
 
     /// Remove info
-    void remove(map_type::const_iterator iter);
-
-    /// Check if the info exists
-    [[nodiscard]] bool exists(const std::string& name) const;
-
-    /// Remove info
     void remove(
       const std::string& name,
-      const std::vector<std::string> input_sockets,
+      const std::vector<std::string>& input_sockets,
       const std::string& output_socket);
 
+    /// Get list of info
+    [[nodiscard]] std::vector<std::shared_ptr<const info_type>> enumerate();
+
     /// Find info
-    [[nodiscard]] std::vector<const info_type*>
+    [[nodiscard]] std::vector<std::shared_ptr<const info_type>>
       find(const std::string& name) const;
 
-    /// cbegin
-    [[nodiscard]] const_iterator cbegin() const;
-
-    /// cend
-    [[nodiscard]] const_iterator cend() const;
+    /// Find info
+    [[nodiscard]] std::vector<std::shared_ptr<const info_type>> find(
+      const std::string& name,
+      const std::vector<std::string>& input,
+      const std::string& output) const;
 
     /// Lock
     [[nodiscard]] std::unique_lock<std::mutex> lock() const;
 
     /// Clear
     void clear();
-
-  private:
-    void remove_iterator(map_type::const_iterator iter);
 
   private:
     /// map
--- src/yave/node/bind_info_manager.cpp
+++ src/yave/node/bind_info_manager.cpp
@@ -4,8 +4,15 @@
 //
 
 #include <yave/node/bind_info_manager.hpp>
+#include <yave/tools/log.hpp>
 
 namespace yave {
+
+  BindInfoManager::BindInfoManager()
+    : m_info {}
+    , m_mtx {}
+  {
+  }
 
   BindInfoManager::BindInfoManager(const BindInfoManager& other)
     : m_info {other.m_info}
@@ -31,44 +38,40 @@ namespace yave {
     return *this;
   }
 
-  BindInfoManager::BindInfoManager()
-    : m_info {}
-    , m_mtx {}
-  {
-  }
-
   [[nodiscard]] bool BindInfoManager::add(const BindInfo& info)
   {
-    auto iter = m_info.emplace(info.name(), info);
+    auto iter = m_info.emplace(info.name(), std::make_shared<info_type>(info));
     return iter != m_info.end();
-  }
-
-  void BindInfoManager::remove_iterator(map_type::const_iterator iter)
-  {
-    m_info.erase(iter);
   }
 
   void BindInfoManager::remove(const std::string& name)
   {
     auto [bgn, end] = m_info.equal_range(name);
-    auto iter       = bgn;
+
+    auto iter = bgn;
     while (iter != end) {
-      remove_iterator(iter++);
+      assert(iter->second->name() == name);
+      m_info.erase(iter++);
     }
   }
 
-  void BindInfoManager::remove(map_type::const_iterator iter)
+  void BindInfoManager::remove(
+    const std::string& name,
+    const std::vector<std::string>& input,
+    const std::string& output)
   {
-    iter = [&]() {
-      for (auto it = m_info.begin(); it != m_info.end(); ++it) {
-        if (iter == it)
-          return iter;
-      }
-      return m_info.cend();
-    }();
-    if (iter == m_info.end())
-      return;
-    remove_iterator(iter);
+    auto [bgn, end] = m_info.equal_range(name);
+
+    auto iter = bgn;
+    while (iter != end) {
+      assert(iter->second->name() == name);
+      if (
+        iter->second->output_socket() == output &&
+        iter->second->input_sockets() == input)
+        m_info.erase(iter++);
+      else
+        ++iter;
+    }
   }
 
   [[nodiscard]] bool BindInfoManager::exists(const std::string& name) const
@@ -76,41 +79,45 @@ namespace yave {
     return m_info.find(name) != m_info.end();
   }
 
-  void BindInfoManager::remove(
-    const std::string& name,
-    const std::vector<std::string> input_sockets,
-    const std::string& output_socket)
+  [[nodiscard]] std::vector<std::shared_ptr<const BindInfoManager::info_type>>
+    BindInfoManager::enumerate()
   {
-
-    auto iter = m_info.begin();
-    auto end  = m_info.end();
-    while (iter != end) {
-      if (
-        iter->second.name() == name &&
-        iter->second.input_sockets() == input_sockets &&
-        iter->second.output_socket() == output_socket) {
-        remove_iterator(iter++);
-      }
+    std::vector<std::shared_ptr<const info_type>> ret;
+    for (auto&& [name, ptr] : m_info) {
+      assert(name == ptr->name());
+      ret.push_back(ptr);
     }
-  }
-
-  [[nodiscard]] std::vector<const BindInfoManager::info_type*>
-    BindInfoManager::find(const std::string& name) const
-  {
-    auto [bgn, end] = m_info.equal_range(name);
-    std::vector<const info_type*> ret;
-    for (auto iter = bgn; iter != end; ++iter) ret.push_back(&iter->second);
     return ret;
   }
 
-  [[nodiscard]] BindInfoManager::const_iterator BindInfoManager::cbegin() const
+  [[nodiscard]] std::vector<std::shared_ptr<const BindInfoManager::info_type>>
+    BindInfoManager::find(const std::string& name) const
   {
-    return m_info.cbegin();
+    auto [bgn, end] = m_info.equal_range(name);
+
+    std::vector<std::shared_ptr<const info_type>> ret;
+    for (auto iter = bgn; iter != end; ++iter) {
+      ret.push_back(iter->second);
+    }
+    return ret;
   }
 
-  [[nodiscard]] BindInfoManager::const_iterator BindInfoManager::cend() const
+  [[nodiscard]] std::vector<std::shared_ptr<const BindInfoManager::info_type>>
+    BindInfoManager::find(
+      const std::string& name,
+      const std::vector<std::string>& input,
+      const std::string& output) const
   {
-    return m_info.cend();
+    auto [bgn, end] = m_info.equal_range(name);
+
+    std::vector<std::shared_ptr<const info_type>> ret;
+    for (auto iter = bgn; iter != end; ++iter) {
+      if (
+        iter->second->output_socket() == output &&
+        iter->second->input_sockets() == input)
+        ret.push_back(iter->second);
+    }
+    return ret;
   }
 
   [[nodiscard]] std::unique_lock<std::mutex> BindInfoManager::lock() const
