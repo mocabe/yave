c1132ee32ae349b34b2178a41f76aa346c159eaf
mocabe <hisa.mocabe@gmail.com>
2019-05-04 03:23:28 +0900

    obj: Add Image

---
+++ include/yave/obj/image.hpp
@@ -0,0 +1,18 @@
+//
+// Copyright (c) 2019 mocabe (https://github.com/mocabe)
+// Distributed under LGPLv3 License. See LICENSE for more details.
+//
+
+#include <yave/image/image.hpp>
+#include <yave/rts/box.hpp>
+
+namespace yave {
+
+  namespace Image {
+    /// Image object
+    using Image = Box<image>;
+  } // namespace Image
+
+} // namespace yave
+
+YAVE_DECL_TYPE(yave::Image::Image, _Image);
