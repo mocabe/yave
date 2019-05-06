312c06b6a2c48df8d094e7e44f1ad5474d854079
mocabe <hisa.mocabe@gmail.com>
2019-05-06 17:24:05 +0900

    submodule: Add boost library

--- external/external.cmake
+++ external/external.cmake
@@ -1,11 +1,12 @@
 function(init_lib_cmake NAME)
-  message(STATUS "init ${NAME}")
+  message(STATUS "Initializing submodule: ${NAME}")
   execute_process(COMMAND git submodule update --init -- external/${NAME}
                   WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 endfunction()
 
 function(init_and_add_lib_cmake NAME)
   init_lib_cmake(${NAME})
+  message(STATUS "Adding library target: ${NAME}")
   add_subdirectory(external/${NAME})
 endfunction()
 
@@ -17,3 +18,17 @@ init_and_add_lib_cmake(spdlog)
 
 # Cathc2
 init_and_add_lib_cmake(Catch2)
+
+# boost
+message(STATUS "Initializing submodule: boost")
+execute_process(COMMAND git submodule update --init --recursive -- external/boost 
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+
+message(STATUS "Building boost library")
+execute_process(COMMAND ./bootstrap.sh WORKING_DIRECTORY ${YAVE_EXTERNAL_DIR}/boost)
+execute_process(COMMAND ./b2 headers WORKING_DIRECTORY ${YAVE_EXTERNAL_DIR}/boost)
+
+message(STATUS "Adding local package: boost")
+set(BOOST_ROOT "${YAVE_EXTERNAL_DIR}/boost" CACHE PATH "Boost submodule" FORCE)
+message(STATUS "Set BOOST_ROOT to ${BOOST_ROOT}")
+find_package(Boost REQUIRED)
