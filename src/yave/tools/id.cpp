04060e8d96f56dd65e001fb38924cb99d6d5a806
mocabe <hisa.mocabe@gmail.com>
2019-05-07 02:27:17 +0900

    tools: use boost/random for ID generator

--- src/yave/tools/id.cpp
+++ src/yave/tools/id.cpp
@@ -5,14 +5,11 @@
 
 #include <yave/tools/id.hpp>
 
+#include <boost/random/random_device.hpp>
+#include <boost/random/mersenne_twister.hpp>
+
 #include <cstring>
 #include <random>
-
-#if defined(__MINGW32__)
-#  include <windows.h>
-#  include <wincrypt.h>
-#  include <system_error>
-#endif
 
 namespace yave {
 
@@ -48,36 +45,10 @@ namespace yave {
 
   uid uid::random_generate()
   {
-#if defined(__MINGW32__)
-    HCRYPTPROV hprov;
-    if (!CryptAcquireContext(&hprov, nullptr, nullptr, PROV_RSA_FULL, 0)) {
-      if (GetLastError() == static_cast<DWORD>(NTE_BAD_KEYSET)) {
-        if (!CryptAcquireContext(
-              &hprov, nullptr, nullptr, PROV_RSA_FULL, CRYPT_NEWKEYSET)) {
-          throw std::system_error(
-            std::error_code(GetLastError(), std::system_category()),
-            "CryptAcquireContext failed.");
-        }
-      } else {
-        throw std::system_error(
-          std::error_code(GetLastError(), std::system_category()),
-          "CryptAcquireContext failed.");
-      }
-    }
-    uint64_t ret;
-    if (!CryptGenRandom(hprov, sizeof(ret), reinterpret_cast<BYTE*>(&ret))) {
-      throw std::system_error(
-        std::error_code(GetLastError(), std::system_category()),
-        "CryptGenRandom failed.");
-    }
-    CryptReleaseContext(hprov, 0);
-    return uid {ret};
-#else
     static_assert(std::is_same_v<uint64_t, std::uint_fast64_t>);
-    std::random_device seed_gen;
-    std::mt19937_64 engine(seed_gen());
+    boost::random::random_device seed_gen;
+    boost::random::mt19937_64 engine(seed_gen());
     return uid {engine()};
-#endif
   }
 
 } // namespace yave
