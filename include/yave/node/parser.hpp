43f13b7c3590e136c1ad16688aec3dff77143bde
mocabe <hisa.mocabe@gmail.com>
2019-05-24 13:03:27 +0900

    parser: Add single header

---
+++ include/yave/node/parser.hpp
@@ -0,0 +1,8 @@
+//
+// Copyright (c) 2019 mocabe (https://github.com/mocabe)
+// Distributed under LGPLv3 License. See LICENSE for more details.
+//
+
+#pragma once
+
+#include <yave/node/parser/node_parser.hpp>
