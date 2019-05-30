cfd05c20deb562b8d742b074e2b726a5e9034f4e
mocabe <hisa.mocabe@gmail.com>
2019-05-30 19:13:34 +0900

    Added useful headers

---
+++ include/yave/node.hpp
@@ -0,0 +1,11 @@
+//
+// Copyright (c) 2019 mocabe (https://github.com/mocabe)
+// Distributed under LGPLv3 License. See LICENSE for more details.
+//
+
+#pragma once
+
+#include <yave/node/core.hpp>
+#include <yave/node/support.hpp>
+#include <yave/node/objects.hpp>
+#include <yave/node/parser.hpp>
---
+++ include/yave/support.hpp
@@ -0,0 +1,13 @@
+//
+// Copyright (c) 2019 mocabe (https://github.com/mocabe)
+// Distributed under LGPLv3 License. See LICENSE for more details.
+//
+
+#pragma once
+
+#include <yave/support/overloaded.hpp>
+#include <yave/support/offset_of_member.hpp>
+#include <yave/support/id.hpp>
+#include <yave/support/id.hpp>
+#include <yave/support/uuid.hpp>
+#include <yave/support/error.hpp>
