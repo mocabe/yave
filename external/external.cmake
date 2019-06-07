875a904de185b008cb2e73cd198986d3368072be
mocabe <hisa.mocabe@gmail.com>
2019-06-08 03:06:43 +0900

    CMake: Add vulkan interface library

--- external/external.cmake
+++ external/external.cmake
@@ -36,6 +36,9 @@ init_and_add_lib_cmake(glfw)
 # ${Vulkan_INCLUDE_DIRS}
 # ${Vulkan_LIBRARIES}
 find_package(Vulkan REQUIRED)
+add_library(vulkan INTERFACE)
+target_include_directories(vulkan INTERFACE ${Vulkan_INCLUDE_DIRS})
+target_link_libraries(vulkan INTERFACE ${Vulkan_LIBRARIES})
 
 # imgui
 init_lib_cmake(imgui)
