712ceec217a2cd265ebbf67b460780acc5a8c607
mocabe <hisa.mocabe@gmail.com>
2019-06-19 14:55:37 +0900

    CMake: search system installed boost first

--- external/external.cmake
+++ external/external.cmake
@@ -76,58 +76,67 @@ add_executable(imgui_demo external/imgui/examples/example_glfw_vulkan/main.cpp)
 target_link_libraries(imgui_demo imgui-glfw-vulkan)
 
 # boost
-message(STATUS "Initializing submodule: boost")
-execute_process(COMMAND git submodule update --init --recursive --jobs 8 external/boost
-                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 
-message(STATUS "Building boost library")
-
-# bootstrap
-if((NOT EXISTS ${YAVE_EXTERNAL_DIR}/boost/bjam.exe OR 
-    NOT EXISTS ${YAVE_EXTERNAL_DIR}/boost/b2.exe) 
-    AND
-   (NOT EXISTS ${YAVE_EXTERNAL_DIR}/boost/bjam OR
-    NOT EXISTS ${YAVE_EXTERNAL_DIR}/boost/b2))
-  execute_process(COMMAND sh bootstrap.sh 
-                  WORKING_DIRECTORY ${YAVE_EXTERNAL_DIR}/boost)
-endif()
-
-# Build Boost library
-if(MINGW)
-  # Use default "mgw" toolset. which uses gcc.
-  # Use workaround for clang to link to gcc binary.
-  execute_process(COMMAND ./b2 -j 8 link=static address-model=64 
-                  WORKING_DIRECTORY ${YAVE_EXTERNAL_DIR}/boost)
-elseif(YAVE_COMPILER_MSVC)
-  # Should link Boost::disable_autolinking on all targets to avoid link errors.
-  execute_process(COMMAND ./b2 -j 8 toolset=msvc link=static address-model=64 
-                  WORKING_DIRECTORY ${YAVE_EXTERNAL_DIR}/boost)
-else()
-  execute_process(COMMAND ./b2 -j 8 toolset=gcc link=static address-model=64 
-                  WORKING_DIRECTORY ${YAVE_EXTERNAL_DIR}/boost)
-endif()
-
-# MinGW clang workaround
-if(MINGW)
-  # get current gcc version
-  execute_process(COMMAND gcc -dumpversion 
-                  OUTPUT_VARIABLE YAVE_GCC_VERSION 
-                  OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-  string(REGEX REPLACE "([0-9]+)\\.([0-9]+)(\\.[0-9]+)?" "\\1" 
-    YAVE_GCC_VERSION_MAJOR "${YAVE_GCC_VERSION}")
-  string(REGEX REPLACE "([0-9]+)\\.([0-9]+)(\\.[0-9]+)?" "\\2" 
-    YAVE_GCC_VERSION_MINOR "${YAVE_GCC_VERSION}")
-
-  # set compiler version
-  set(YAVE_GCC_VERSION_TAG "${YAVE_GCC_VERSION_MAJOR}${YAVE_GCC_VERSION_MINOR}")
-  set(Boost_COMPILER "-mgw${YAVE_GCC_VERSION_TAG}")
-endif()
-
-# Let FindBoost create Boost targets
 message(STATUS "Find package: boost")
-set(BOOST_ROOT "${YAVE_EXTERNAL_DIR}/boost" CACHE PATH "" FORCE)
+
+set(YAVE_BOOST_COMPONENTS random program_options filesystem)
 set (Boost_ARCHITECTURE "-x64")
 set(Boost_USE_STATIC_LIBS ON)
-set(Boost_NO_SYSTEM_PATHS TRUE)
-find_package(Boost COMPONENTS random program_options filesystem REQUIRED)
+find_package(Boost 1.70.0 COMPONENTS ${YAVE_BOOST_COMPONENTS})
+
+if(NOT Boost_FOUND)
+
+  message(STATUS "Initializing submodule: boost")
+  execute_process(COMMAND git submodule update --init --recursive --jobs 8 external/boost
+                  WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+  
+  message(STATUS "Building boost library")
+  
+  # bootstrap
+  if((NOT EXISTS ${YAVE_EXTERNAL_DIR}/boost/bjam.exe OR 
+      NOT EXISTS ${YAVE_EXTERNAL_DIR}/boost/b2.exe) 
+      AND
+     (NOT EXISTS ${YAVE_EXTERNAL_DIR}/boost/bjam OR
+      NOT EXISTS ${YAVE_EXTERNAL_DIR}/boost/b2))
+    execute_process(COMMAND sh bootstrap.sh 
+                    WORKING_DIRECTORY ${YAVE_EXTERNAL_DIR}/boost)
+  endif()
+  
+  # Build Boost library
+  if(MINGW)
+    # Use default "mgw" toolset. which uses gcc.
+    # Use workaround for clang to link to gcc binary.
+    execute_process(COMMAND ./b2 -j 8 link=static address-model=64 
+                    WORKING_DIRECTORY ${YAVE_EXTERNAL_DIR}/boost)
+  elseif(YAVE_COMPILER_MSVC)
+    # Should link Boost::disable_autolinking on all targets to avoid link errors.
+    execute_process(COMMAND ./b2 -j 8 toolset=msvc link=static address-model=64 
+                    WORKING_DIRECTORY ${YAVE_EXTERNAL_DIR}/boost)
+  else()
+    execute_process(COMMAND ./b2 -j 8 toolset=gcc link=static address-model=64 
+                    WORKING_DIRECTORY ${YAVE_EXTERNAL_DIR}/boost)
+  endif()
+  
+  # MinGW clang workaround
+  if(MINGW)
+    # get current gcc version
+    execute_process(COMMAND gcc -dumpversion 
+                    OUTPUT_VARIABLE YAVE_GCC_VERSION 
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+  
+    string(REGEX REPLACE "([0-9]+)\\.([0-9]+)(\\.[0-9]+)?" "\\1" 
+      YAVE_GCC_VERSION_MAJOR "${YAVE_GCC_VERSION}")
+    string(REGEX REPLACE "([0-9]+)\\.([0-9]+)(\\.[0-9]+)?" "\\2" 
+      YAVE_GCC_VERSION_MINOR "${YAVE_GCC_VERSION}")
+  
+    # set compiler version
+    set(YAVE_GCC_VERSION_TAG "${YAVE_GCC_VERSION_MAJOR}${YAVE_GCC_VERSION_MINOR}")
+    set(Boost_COMPILER "-mgw${YAVE_GCC_VERSION_TAG}")
+  endif()
+  
+  # Let FindBoost create Boost targets
+  set(BOOST_ROOT "${YAVE_EXTERNAL_DIR}/boost" CACHE PATH "" FORCE)
+  set(Boost_NO_SYSTEM_PATHS TRUE)
+  find_package(Boost COMPONENTS random program_options filesystem REQUIRED)
+
+endif()
