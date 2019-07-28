4a8ac2ddd72f292df053d18f930afb528941db37
mocabe <hisa.mocabe@gmail.com>
2019-07-28 21:06:40 +0900

    blend_op: Implement missing blend func

--- include/yave/node/obj/blend_op.hpp
+++ include/yave/node/obj/blend_op.hpp
@@ -8,6 +8,7 @@
 #include <yave/node/obj/function.hpp>
 #include <yave/node/obj/compositor.hpp>
 #include <yave/data/lib/blend_operation.hpp>
+#include <yave/data/lib/image_blend.hpp>
 
 namespace yave {
 
@@ -68,6 +69,7 @@ namespace yave {
       get_blend_op_node_info(blend_operation::dst),
       get_blend_op_node_info(blend_operation::over),
       get_blend_op_node_info(blend_operation::in),
+      get_blend_op_node_info(blend_operation::out),
       get_blend_op_node_info(blend_operation::add),
     };
     return ret;
@@ -81,6 +83,7 @@ namespace yave {
       get_blend_op_bind_info(blend_operation::dst),
       get_blend_op_bind_info(blend_operation::over),
       get_blend_op_bind_info(blend_operation::in),
+      get_blend_op_bind_info(blend_operation::out),
       get_blend_op_bind_info(blend_operation::add),
     };
     return ret;
@@ -91,7 +94,7 @@ namespace yave {
   {
     return_type code()
     {
-      return arg<0>();
+      return eval_arg<0>();
     }
   };
 
@@ -119,7 +122,7 @@ namespace yave {
   {
     return_type code()
     {
-      return arg<1>();
+      return eval_arg<1>();
     }
   };
 
@@ -138,6 +141,210 @@ namespace yave {
         "out",
         make_object<InstanceGetterFunction<BlendOpDst>>(),
         "BlendOpDst");
+    }
+  };
+
+  struct BlendOpOver
+    : NodeFunction<BlendOpOver, FrameBuffer, FrameBuffer, FrameBuffer>
+  {
+    return_type code() const
+    {
+      auto fb_src = eval_arg<0>();
+      auto fb_dst = eval_arg<1>();
+
+      // clone dst
+      auto dst = fb_dst->copy();
+
+      const_image_view src_view   = fb_src->get_image_view();
+      mutable_image_view dst_view = dst->get_image_view();
+
+      if (src_view.image_format() == image_format::RGBA8UI) {
+        alpha_blend_RGBA8UI(src_view, dst_view, blend_operation::over);
+        return dst;
+      }
+
+      if (src_view.image_format() == image_format::RGBA16UI) {
+        alpha_blend_RGBA16UI(src_view, dst_view, blend_operation::over);
+        return dst;
+      }
+
+      if (src_view.image_format() == image_format::RGBA32F) {
+        alpha_blend_RGBA32F(src_view, dst_view, blend_operation::over);
+        return dst;
+      }
+
+      throw std::runtime_error("Invalid frame buffer type");
+    }
+  };
+
+  template <>
+  struct node_function_info_traits<BlendOpOver>
+  {
+    static node_info get_node_info()
+    {
+      return node_info("BlendOpOver", {"src", "dst"}, {"out"});
+    }
+    static bind_info get_bind_info()
+    {
+      return bind_info(
+        "BlendOpOver",
+        {"src", "dst"},
+        "out",
+        make_object<InstanceGetterFunction<BlendOpOver>>(),
+        "BlendOpOver");
+    }
+  };
+
+  struct BlendOpIn
+    : NodeFunction<BlendOpIn, FrameBuffer, FrameBuffer, FrameBuffer>
+  {
+    return_type code() const
+    {
+      auto fb_src = eval_arg<0>();
+      auto fb_dst = eval_arg<1>();
+
+      // clone dst
+      auto dst = fb_dst->copy();
+
+      const_image_view src_view   = fb_src->get_image_view();
+      mutable_image_view dst_view = dst->get_image_view();
+
+      if (src_view.image_format() == image_format::RGBA8UI) {
+        alpha_blend_RGBA8UI(src_view, dst_view, blend_operation::in);
+        return dst;
+      }
+
+      if (src_view.image_format() == image_format::RGBA16UI) {
+        alpha_blend_RGBA16UI(src_view, dst_view, blend_operation::in);
+        return dst;
+      }
+
+      if (src_view.image_format() == image_format::RGBA32F) {
+        alpha_blend_RGBA32F(src_view, dst_view, blend_operation::in);
+        return dst;
+      }
+
+      throw std::runtime_error("Invalid frame buffer type");
+    }
+  };
+
+  template <>
+  struct node_function_info_traits<BlendOpIn>
+  {
+    static node_info get_node_info()
+    {
+      return node_info("BlendOpIn", {"src", "dst"}, {"out"});
+    }
+    static bind_info get_bind_info()
+    {
+      return bind_info(
+        "BlendOpIn",
+        {"src", "dst"},
+        "out",
+        make_object<InstanceGetterFunction<BlendOpIn>>(),
+        "BlendOpIn");
+    }
+  };
+
+  struct BlendOpOut
+    : NodeFunction<BlendOpOut, FrameBuffer, FrameBuffer, FrameBuffer>
+  {
+    return_type code() const
+    {
+      auto fb_src = eval_arg<0>();
+      auto fb_dst = eval_arg<1>();
+
+      // clone dst
+      auto dst = fb_dst->copy();
+
+      const_image_view src_view   = fb_src->get_image_view();
+      mutable_image_view dst_view = dst->get_image_view();
+
+      if (src_view.image_format() == image_format::RGBA8UI) {
+        alpha_blend_RGBA8UI(src_view, dst_view, blend_operation::out);
+        return dst;
+      }
+
+      if (src_view.image_format() == image_format::RGBA16UI) {
+        alpha_blend_RGBA16UI(src_view, dst_view, blend_operation::out);
+        return dst;
+      }
+
+      if (src_view.image_format() == image_format::RGBA32F) {
+        alpha_blend_RGBA32F(src_view, dst_view, blend_operation::out);
+        return dst;
+      }
+
+      throw std::runtime_error("Invalid frame buffer type");
+    }
+  };
+
+  template <>
+  struct node_function_info_traits<BlendOpOut>
+  {
+    static node_info get_node_info()
+    {
+      return node_info("BlendOpOut", {"src", "dst"}, {"out"});
+    }
+    static bind_info get_bind_info()
+    {
+      return bind_info(
+        "BlendOpOut",
+        {"src", "dst"},
+        "out",
+        make_object<InstanceGetterFunction<BlendOpOut>>(),
+        "BlendOpOut");
+    }
+  };
+
+  struct BlendOpAdd
+    : NodeFunction<BlendOpAdd, FrameBuffer, FrameBuffer, FrameBuffer>
+  {
+    return_type code() const
+    {
+      auto fb_src = eval_arg<0>();
+      auto fb_dst = eval_arg<1>();
+
+      // clone dst
+      auto dst = fb_dst->copy();
+
+      const_image_view src_view   = fb_src->get_image_view();
+      mutable_image_view dst_view = dst->get_image_view();
+
+      if (src_view.image_format() == image_format::RGBA8UI) {
+        alpha_blend_RGBA8UI(src_view, dst_view, blend_operation::add);
+        return dst;
+      }
+
+      if (src_view.image_format() == image_format::RGBA16UI) {
+        alpha_blend_RGBA16UI(src_view, dst_view, blend_operation::add);
+        return dst;
+      }
+
+      if (src_view.image_format() == image_format::RGBA32F) {
+        alpha_blend_RGBA32F(src_view, dst_view, blend_operation::add);
+        return dst;
+      }
+
+      throw std::runtime_error("Invalid frame buffer type");
+    }
+  };
+
+  template <>
+  struct node_function_info_traits<BlendOpAdd>
+  {
+    static node_info get_node_info()
+    {
+      return node_info("BlendOpAdd", {"src", "dst"}, {"out"});
+    }
+    static bind_info get_bind_info()
+    {
+      return bind_info(
+        "BlendOpAdd",
+        {"src", "dst"},
+        "out",
+        make_object<InstanceGetterFunction<BlendOpAdd>>(),
+        "BlendOpAdd");
     }
   };
 } // namespace yave
