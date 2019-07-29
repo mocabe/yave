e0cce3d9cb5a073eba2e466ddf56b50a741a6cd5
mocabe <hisa.mocabe@gmail.com>
2019-07-29 14:02:05 +0900

    LayerCompositor: Move impl to backend

---
+++ include/yave/backend/default/render/compositor.hpp
@@ -0,0 +1,57 @@
+//
+// Copyright (c) 2019 mocabe (https://github.com/mocabe)
+// Distributed under LGPLv3 License. See LICENSE for more details.
+//
+
+#pragma once
+
+#include <yave/node/obj/compositor.hpp>
+
+#include <yave/node/obj/function.hpp>
+#include <yave/data/obj/frame_buffer.hpp>
+#include <yave/node/obj/instance_getter.hpp>
+#include <yave/backend/default/system/config.hpp>
+
+namespace yave {
+
+  namespace backend::default_render {
+
+    struct LayerBlendOp
+      : NodeFunction<LayerBlendOp, FrameBuffer, FrameBuffer, FrameBuffer>
+    {
+      // just for type
+    };
+
+    /// Compositor
+    struct LayerCompositor : NodeFunction<
+                               LayerCompositor,
+                               FrameBuffer,  // src
+                               FrameBuffer,  // dst
+                               LayerBlendOp, // blend op
+                               FrameBuffer>
+    {
+      return_type code() const
+      {
+        // apply blend function
+        return arg<2>() << arg<0>() << arg<1>();
+      }
+    };
+
+  } // namespace backend::default_render
+
+  template <>
+  struct bind_info_traits<LayerCompositor, backend::tags::default_render>
+  {
+    static bind_info get_bind_info()
+    {
+      return bind_info(
+        "LayerCompositor",
+        {"src", "dst", "blend op"},
+        "out",
+        make_object<
+          InstanceGetterFunction<backend::default_render::LayerCompositor>>(),
+        "LayerCompositor");
+    }
+  };
+
+} // namespace yave
--- include/yave/node/obj/compositor.hpp
+++ include/yave/node/obj/compositor.hpp
@@ -5,50 +5,20 @@
 
 #pragma once
 
-#include <yave/node/obj/function.hpp>
-#include <yave/data/obj/frame_buffer.hpp>
-#include <yave/node/obj/instance_getter.hpp>
 #include <yave/node/core/get_info.hpp>
 
 namespace yave {
 
-  struct LayerBlendOp
-    : NodeFunction<LayerBlendOp, FrameBuffer, FrameBuffer, FrameBuffer>
-  {
-    // just for type
-  };
-
-  /// Compositor
-  struct LayerCompositor : NodeFunction<
-                             LayerCompositor,
-                             FrameBuffer,  // src
-                             FrameBuffer,  // dst
-                             LayerBlendOp, // blend op
-                             FrameBuffer>
-  {
-    return_type code() const
-    {
-      // apply blend function
-      return arg<2>() << arg<0>() << arg<1>();
-    }
-  };
+  /// Layer compositor node
+  struct LayerCompositor;
 
   // info traits
   template <>
-  struct node_function_info_traits<LayerCompositor>
+  struct node_info_traits<LayerCompositor>
   {
     static node_info get_node_info()
     {
       return node_info("LayerCompositor", {"src", "dst", "blend op"}, {"out"});
-    }
-    static bind_info get_bind_info()
-    {
-      return bind_info(
-        "LayerCompositor",
-        {"src", "dst", "blend op"},
-        "out",
-        make_object<InstanceGetterFunction<LayerCompositor>>(),
-        "LayerCompositor");
     }
   };
 
