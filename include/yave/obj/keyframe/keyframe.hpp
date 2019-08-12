c9f49cf3197f8bf07906cb3de5cf604be88bf431
mocabe <hisa.mocabe@gmail.com>
2019-08-12 23:11:07 +0900

    keyframe: Add basic object definitions

---
+++ include/yave/obj/keyframe/keyframe.hpp
@@ -0,0 +1,28 @@
+//
+// Copyright (c) 2019 mocabe (https://github.com/mocabe)
+// Distributed under LGPLv3 License. See LICENSE for more details.
+//
+
+#pragma once
+
+#include <yave/lib/keyframe/keyframe.hpp>
+#include <yave/rts/box.hpp>
+
+namespace yave {
+
+  /// Templated keyframe object
+  template <class T>
+  using Keyframe = Box<keyframe<T>>;
+
+  /// Object type of keyframe<int32_t>
+  using KeyframeInt = Keyframe<int32_t>;
+  /// Object type of keyframe<float>
+  using KeyframeFloat = Keyframe<float>;
+  /// Object type of keyframe<bool>
+  using KeyframeBool = Keyframe<bool>;
+
+} // namespace yave
+
+YAVE_DECL_TYPE(yave::KeyframeInt, "bf4cb7db-32e8-4bc1-bc9e-c936cb53dac3");
+YAVE_DECL_TYPE(yave::KeyframeFloat, "66d7b6cc-167a-4630-87aa-50ac1b156b4e");
+YAVE_DECL_TYPE(yave::KeyframeBool, "427bf0f3-ec15-4413-ae3b-964ce41647bf");
