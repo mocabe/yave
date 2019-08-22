a511adb4eaf2d4ef7b8f63393051ca3d80308b11
mocabe <hisa.mocabe@gmail.com>
2019-08-22 15:56:28 +0900

    keyframe: Add handles for float values

---
+++ include/yave/lib/keyframe/float.hpp
@@ -0,0 +1,34 @@
+//
+// Copyright (c) 2019 mocabe (https://github.com/mocabe)
+// Distributed under LGPLv3 License. See LICENSE for more details.
+//
+
+#pragma once
+
+#include <yave/config/config.hpp>
+#include <yave/lib/time/time.hpp>
+#include <yave/lib/keyframe/keyframe.hpp>
+
+namespace yave {
+
+  /// Data struct for KeyframeFloat
+  struct kf_float_handles
+  {
+    /// Control point for KeyframeFloat
+    struct control_point
+    {
+      /// time axis position
+      time time;
+      /// value
+      double value;
+    };
+
+    /// lhs control point
+    control_point cp_left;
+    /// rhs control point
+    control_point cp_right;
+    /// value
+    double value;
+  };
+
+} // namespace yave
