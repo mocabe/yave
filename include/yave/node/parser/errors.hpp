75c8c1be961348addeb35d747025cd1b06a7df67
mocabe <hisa.mocabe@gmail.com>
2019-08-27 18:44:07 +0900

    Remove type errors from parser_error namespace, add unexpected_error.

--- include/yave/node/parser/errors.hpp
+++ include/yave/node/parser/errors.hpp
@@ -15,7 +15,7 @@
 
 namespace yave {
 
-  namespace parse_errors {
+  namespace parse_error {
 
     struct no_sufficient_input : error_info<no_sufficient_input>
     {
@@ -37,118 +37,19 @@ namespace yave {
       node_handle m_node;
     };
 
-    /// No valid overloading error.
-    struct no_valid_overloading : error_info<no_valid_overloading>
+    struct unexpected_error : error_info<unexpected_error>
     {
-      no_valid_overloading(
-        const node_handle& node,
-        const std::vector<std::shared_ptr<const bind_info>>& binds)
-        : m_node {node}
-        , m_binds {binds}
+      unexpected_error(const std::string& msg)
+        : m_msg {msg}
       {
       }
 
-      /// Get error message.
+      /// Error message.
       [[nodiscard]] virtual std::string message() const override;
 
-      /// Get node.
-      [[nodiscard]] const node_handle& node() const
-      {
-        return m_node;
-      }
-
-      /// Get overloading bindings.
-      [[nodiscard]] const std::vector<std::shared_ptr<const bind_info>>
-        binds() const
-      {
-        return m_binds;
-      }
-
     private:
-      node_handle m_node;
-      std::vector<std::shared_ptr<const bind_info>> m_binds;
+      std::string m_msg;
     };
 
-    /// Ambiguous overloading error.
-    struct ambiguous_overloading : error_info<ambiguous_overloading>
-    {
-      ambiguous_overloading(
-        const node_handle& node,
-        const std::vector<std::shared_ptr<const bind_info>>& binds)
-        : m_node {node}
-        , m_binds {binds}
-      {
-      }
-
-      /// Get error message.
-      [[nodiscard]] virtual std::string message() const override;
-
-      /// Get node.
-      [[nodiscard]] const node_handle& node() const
-      {
-        return m_node;
-      }
-
-      /// Get overloading bindings.
-      [[nodiscard]] const std::vector<std::shared_ptr<const bind_info>>&
-        binds() const
-      {
-        return m_binds;
-      }
-
-    private:
-      node_handle m_node;
-      std::vector<std::shared_ptr<const bind_info>> m_binds;
-    };
-
-    /// Type missmatch error.
-    struct type_missmatch : error_info<type_missmatch>
-    {
-      type_missmatch(
-        const node_handle& node,
-        const std::string& socket,
-        const object_ptr<const Type> expected,
-        const object_ptr<const Type>& provided)
-        : m_node {node}
-        , m_socket {socket}
-        , m_expected {expected}
-        , m_provided {provided}
-      {
-      }
-
-      /// Get error message.
-      [[nodiscard]] virtual std::string message() const override;
-
-      /// Get node.
-      [[nodiscard]] const node_handle& node() const
-      {
-        return m_node;
-      }
-
-      /// Get socket.
-      [[nodiscard]] const std::string& socket() const
-      {
-        return m_socket;
-      }
-
-      /// Get expected type.
-      [[nodiscard]] object_ptr<const Type> expected() const
-      {
-        return m_expected;
-      }
-
-      /// Get provided type.
-      [[nodiscard]] object_ptr<const Type> provided() const
-      {
-        return m_provided;
-      }
-
-    private:
-      node_handle m_node;
-      std::string m_socket;
-      object_ptr<const Type> m_expected;
-      object_ptr<const Type> m_provided;
-    };
-
-  } // namespace parse_errors
+  } // namespace parse_error
 } // namespace yave
