6516704d818948db3fa38e1e13b835b1ff39c73d
mocabe <hisa.mocabe@gmail.com>
2019-08-28 15:22:55 +0900

    Add more tests

--- test/node/parser/parser.cpp
+++ test/node/parser/parser.cpp
@@ -14,6 +14,7 @@ TEST_CASE("node_parser unit test")
   node_parser parser;
 
   auto prim_info = node_info("prim", {}, {"value"}, true);
+  auto norm_info = node_info("node", {"0", "1"}, {"0"}, false);
 
   SECTION("prim")
   {
@@ -30,5 +31,50 @@ TEST_CASE("node_parser unit test")
     REQUIRE(parsed->graph.get_info(parsed_root) == prim_info);
     REQUIRE(parsed->graph.get_primitive(parsed_root));
     REQUIRE(parsed->graph.get_primitive(parsed_root) == primitive_t(42));
+  }
+
+  SECTION("norm")
+  {
+    auto prim = graph.add(prim_info, 42);
+    auto norm = graph.add(norm_info);
+
+    REQUIRE(prim);
+    REQUIRE(norm);
+
+    SECTION("prim")
+    {
+      auto parsed = parser.parse(graph, prim);
+      REQUIRE(parsed);
+      REQUIRE(parsed->graph.nodes().size() == 1);
+    }
+
+    SECTION("not sufficient")
+    {
+      auto parsed = parser.parse(graph, norm);
+      REQUIRE(!parsed);
+    }
+
+    SECTION("single")
+    {
+      auto c = graph.connect(prim, "value", norm, "0");
+      REQUIRE(c);
+      auto parsed = parser.parse(graph, norm);
+      REQUIRE(parsed);
+      REQUIRE(parsed->graph.nodes().size() == 2);
+      REQUIRE(parser.get_errors().empty());
+    }
+
+    SECTION("multi")
+    {
+      auto c0 = graph.connect(prim, "value", norm, "0");
+      auto c1 = graph.connect(prim, "value", norm, "1");
+
+      REQUIRE(c0);
+      REQUIRE(c1);
+
+      auto parsed = parser.parse(graph, norm);
+      REQUIRE(parsed);
+      REQUIRE(parsed->graph.nodes().size() == 2);
+    }
   }
 }
