efc1d24a07d48bc42c53606729521711b8aa59e6
mocabe <hisa.mocabe@gmail.com>
2019-08-31 14:17:18 +0900

    node::FrameBuffer: Fix primitivity

--- include/yave/node/obj/frame_buffer.hpp
+++ include/yave/node/obj/frame_buffer.hpp
@@ -19,7 +19,8 @@ namespace yave {
   {
     static node_info get_node_info()
     {
-      return node_info("FrameBuffer", {}, {"value"});
+      // pseudo primitive
+      return node_info("FrameBuffer", {}, {"value"}, true);
     }
   };
 } // namespace yave
