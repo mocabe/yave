b2d4c777d88f3b7a0a45b901ee0883e270478bdc
mocabe <hisa.mocabe@gmail.com>
2019-10-15 04:36:55 +0900

    Add parent node handle to socket_info

--- src/yave/node/core/socket_info.cpp
+++ src/yave/node/core/socket_info.cpp
@@ -7,9 +7,10 @@
 
 namespace yave {
 
-  socket_info::socket_info(std::string name, socket_type type)
+  socket_info::socket_info(std::string name, socket_type type, node_handle node)
     : m_name {std::move(name)}
     , m_type {std::move(type)}
+    , m_node {std::move(node)}
   {
   }
 
@@ -21,6 +22,11 @@ namespace yave {
   auto socket_info::type() const -> socket_type
   {
     return m_type;
+  }
+
+  auto socket_info::node() const -> const node_handle&
+  {
+    return m_node;
   }
 
 } // namespace yave
