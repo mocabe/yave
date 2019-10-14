b1a2b09b8c3edc7a9d3efb4a062ca93d6147814a
mocabe <hisa.mocabe@gmail.com>
2019-10-15 05:06:25 +0900

    connection_info: Now have socket_handle instead of std::string

--- src/yave/node/core/connection_info.cpp
+++ src/yave/node/core/connection_info.cpp
@@ -9,9 +9,9 @@ namespace yave {
 
   connection_info::connection_info(
     const node_handle& src_node,
-    const std::string& src_socket,
+    const socket_handle& src_socket,
     const node_handle& dst_node,
-    const std::string& dst_socket)
+    const socket_handle& dst_socket)
     : m_src_node {src_node}
     , m_src_socket {src_socket}
     , m_dst_node {dst_node}
@@ -21,9 +21,9 @@ namespace yave {
 
   connection_info::connection_info(
     const node_handle& src_node,
-    const std::string& src_socket,
+    const socket_handle& src_socket,
     const node_handle& dst_node,
-    const std::string& dst_socket,
+    const socket_handle& dst_socket,
     const std::vector<node_handle>& src_interfaces,
     const std::vector<node_handle>& dst_interfaces)
     : m_src_node {src_node}
@@ -40,7 +40,7 @@ namespace yave {
     return m_src_node;
   }
 
-  auto connection_info::src_socket() const -> const std::string&
+  auto connection_info::src_socket() const -> const socket_handle&
   {
     return m_src_socket;
   }
@@ -50,7 +50,7 @@ namespace yave {
     return m_dst_node;
   }
 
-  auto connection_info::dst_socket() const -> const std::string&
+  auto connection_info::dst_socket() const -> const socket_handle&
   {
     return m_dst_socket;
   }
@@ -65,19 +65,6 @@ namespace yave {
     -> const std::vector<node_handle>&
   {
     return m_dst_interfaces;
-  }
-
-  bool operator==(const connection_info& lhs, const connection_info& rhs)
-  {
-    return lhs.m_src_node == rhs.m_src_node &&
-           lhs.m_src_socket == rhs.m_src_socket &&
-           lhs.m_dst_node == rhs.m_dst_node &&
-           lhs.m_dst_socket == rhs.m_dst_socket;
-  }
-
-  bool operator!=(const connection_info& lhs, const connection_info& rhs)
-  {
-    return !(lhs == rhs);
   }
 
 } // namespace yave
