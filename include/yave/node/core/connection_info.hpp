d1db6fd8ffd25b04eacc4f17d182d6af422afc48
mocabe <hisa.mocabe@gmail.com>
2019-10-15 05:06:25 +0900

    connection_info: Now have socket_handle instead of std::string

--- include/yave/node/core/connection_info.hpp
+++ include/yave/node/core/connection_info.hpp
@@ -6,6 +6,7 @@
 #pragma once
 
 #include <yave/node/core/node_handle.hpp>
+#include <yave/node/core/socket_handle.hpp>
 
 namespace yave {
 
@@ -18,15 +19,15 @@ namespace yave {
     connection_info(connection_info&&)      = default;
     connection_info(
       const node_handle& src_node,
-      const std::string& src_socket,
+      const socket_handle& src_socket,
       const node_handle& dst_node,
-      const std::string& dst_socket);
+      const socket_handle& dst_socket);
 
     connection_info(
       const node_handle& src_node,
-      const std::string& src_socket,
+      const socket_handle& src_socket,
       const node_handle& dst_node,
-      const std::string& dst_socket,
+      const socket_handle& dst_socket,
       const std::vector<node_handle>& src_interfaces,
       const std::vector<node_handle>& dst_interfaces);
 
@@ -34,13 +35,13 @@ namespace yave {
     [[nodiscard]] auto src_node() const -> const node_handle&;
 
     /// Get socket of source node.
-    [[nodiscard]] auto src_socket() const -> const std::string&;
+    [[nodiscard]] auto src_socket() const -> const socket_handle&;
 
     /// Get dest node.
     [[nodiscard]] auto dst_node() const -> const node_handle&;
 
     /// Get socket of dest noed.
-    [[nodiscard]] auto dst_socket() const -> const std::string&;
+    [[nodiscard]] auto dst_socket() const -> const socket_handle&;
 
     /// Get interface nodes on src socket
     [[nodiscard]] auto src_interfaces() const
@@ -50,23 +51,15 @@ namespace yave {
     [[nodiscard]] auto dst_interfaces() const
       -> const std::vector<node_handle>&;
 
-    friend bool operator==(
-      const connection_info& lhs,
-      const connection_info& rhs);
-
-    friend bool operator!=(
-      const connection_info& lhs,
-      const connection_info& rhs);
-
   private:
     /// source node
     node_handle m_src_node;
     /// source socket
-    std::string m_src_socket;
+    socket_handle m_src_socket;
     /// destination node
     node_handle m_dst_node;
     /// destination socket
-    std::string m_dst_socket;
+    socket_handle m_dst_socket;
 
   private:
     /// interface nodes sharing src socket
@@ -74,15 +67,5 @@ namespace yave {
     /// interface nodes sharing dst socket
     std::vector<node_handle> m_dst_interfaces;
   };
-
-  /// operator==
-  [[nodiscard]] bool operator==(
-    const connection_info& lhs,
-    const connection_info& rhs);
-
-  /// operator!=
-  [[nodiscard]] bool operator!=(
-    const connection_info& lhs,
-    const connection_info& rhs);
 
 } // namespace yave
