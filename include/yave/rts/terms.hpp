3bec28ee6fb2065446990857adb0e3d37812a6c2
mocabe <hisa.mocabe@gmail.com>
2019-10-19 01:36:05 +0900

    Add tm_list

--- include/yave/rts/terms.hpp
+++ include/yave/rts/terms.hpp
@@ -44,6 +44,12 @@ namespace yave {
   {
   };
 
+  /// tm_list
+  template <class T>
+  struct tm_list
+  {
+  };
+
   // ------------------------------------------
   // Term accessors
 
@@ -98,6 +104,16 @@ namespace yave {
     constexpr size_t size() const
     {
       return sizeof...(Ts);
+    }
+  };
+
+  template <class T>
+  struct meta_type<tm_list<T>>
+  {
+    using type = tm_list<T>;
+    constexpr size_t t() const
+    {
+      return type_c<T>;
     }
   };
 
@@ -299,18 +315,50 @@ namespace yave {
 
   } // namespace detail
 
-  /// is_varvalue_v
+  /// is_tm_varvalue
   template <class T>
   [[nodiscard]] constexpr auto is_tm_varvalue(meta_type<T>)
   {
     return detail::is_tm_varvalue_impl<T>::value;
   }
 
-  /// has_varvalue_v
+  /// has_tm_varvalue_v
   template <class T>
   [[nodiscard]] constexpr auto has_tm_varvalue()
   {
-    return is_varvalue(get_term<T>());
+    return is_tm_varvalue(get_term<T>());
+  }
+
+  // ------------------------------------------
+  // is_tm_list
+
+  namespace detail {
+
+    template <class T>
+    struct is_tm_list_impl
+    {
+      static constexpr auto value = false_c;
+    };
+
+    template <class T>
+    struct is_tm_list_impl<tm_list<T>>
+    {
+      static constexpr auto value = true_c;
+    };
+  }
+
+  /// is_tm_list
+  template <class T>
+  [[nodiscard]] constexpr auto is_tm_list(meta_type<T>)
+  {
+    return detail::is_tm_list_impl<T>::value;
+  }
+
+  /// has_tm_list
+  template <class T>
+  [[nodiscard]] constexpr auto has_tm_list()
+  {
+    return is_tm_list(get_term<T>());
   }
 
   // ------------------------------------------
@@ -356,6 +404,15 @@ namespace yave {
   [[nodiscard]] constexpr auto make_tm_var(meta_type<Tag>)
   {
     return type_c<tm_var<Tag>>;
+  }
+
+  // ------------------------------------------
+  // make_tm_list
+
+  template <class T>
+  [[nodiscard]] constexpr auto make_tm_list(meta_type<T>)
+  {
+    return type_c<tm_list<T>>;
   }
 
   // ------------------------------------------
