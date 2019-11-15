eafa953f0b1e57f526d10f904ce7b038869201b9
mocabe <hisa.mocabe@gmail.com>
2019-11-15 18:28:55 +0900

    imgui: Add more operators

--- include/yave/lib/imgui/ext_vec2_ops.hpp
+++ include/yave/lib/imgui/ext_vec2_ops.hpp
@@ -20,4 +20,23 @@ namespace yave::imgui {
     return ImVec2(lhs.x - rhs.x, lhs.y - rhs.y);
   }
 
+  inline ImVec2 operator*(const ImVec2& vec, float scalar)
+  {
+    return ImVec2(vec.x * scalar, vec.y * scalar);
+  }
+
+  inline ImVec2 operator/(const ImVec2& vec, float scalar)
+  {
+    return ImVec2(vec.x / scalar, vec.y / scalar);
+  }
+
+  inline bool operator==(const ImVec2& lhs, const ImVec2& rhs)
+  {
+    return lhs.x == rhs.x && lhs.y == rhs.y;
+  }
+
+  inline bool operator!=(const ImVec2& lhs, const ImVec2& rhs)
+  {
+    return !(lhs == rhs);
+  }
 } // namespace yave
