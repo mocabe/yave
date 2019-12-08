mocabe/yave

commit 53eb83e2a69d49315cb2707d5e48aff0bca4b186

rts: Destructive graph reduction (#10)
* Going back to destructive apply cache  
* Hybrid caching using PAP and values  
* Self updating model  
* Eval tests

include/yave/rts/dynamic_typing.hpp
@@ -12,6 +12,7 @@
 #include <yave/rts/utility.hpp>
 #include <yave/rts/undefined.hpp>
 #include <yave/rts/object_util.hpp>
+#include <yave/rts/closure.hpp>
 
 #include <vector>
 
@@ -417,9 +418,13 @@ namespace yave {
         // Apply
         if (auto apply = value_cast_if<const Apply>(obj)) {
           auto& apply_storage = _get_storage(*apply);
-          auto _t1            = rec_app(apply_storage.app());
-          auto _t2            = rec(apply_storage.arg());
-          auto _t             = genvar();
+          // cached
+          if (apply_storage.is_result()) {
+            return rec(apply_storage.get_result());
+          }
+          auto _t1 = rec_app(apply_storage.app());
+          auto _t2 = rec(apply_storage.arg());
+          auto _t  = genvar();
           auto c =
             std::vector {Constr {_t1, make_object<Type>(arrow_type {_t2, _t})}};
           auto s = unify(std::move(c), obj);
@@ -431,9 +436,16 @@ namespace yave {
         // var -> var
         if (has_var_type(obj))
           return get_type(obj);
-        // arrow -> genpoly arrow
-        if (has_arrow_type(obj))
+        // arrow -> arrow or PAP
+        if (has_arrow_type(obj)) {
+          auto c = reinterpret_cast<const Closure<>*>(obj.get());
+          // pap: return root apply node
+          if (c->is_pap()) {
+            return rec(c->vertebrae(0));
+          }
+          // arrow: get_type
           return get_type(obj);
+        }
 
         unreachable();
       }

include/yave/rts/apply.hpp
@@ -22,46 +22,39 @@ namespace yave {
     {
     }
 
-    const auto& app() const noexcept
+    auto& app() const noexcept
     {
-      assert(!evaluated());
+      assert(!is_result());
       return m_app;
     }
 
-    const auto& arg() const noexcept
+    auto& arg() const noexcept
     {
-      assert(!evaluated());
+      assert(!is_result());
       return m_arg;
     }
 
-    bool evaluated() const noexcept
+    bool is_result() const noexcept
     {
-      return static_cast<bool>(m_cache);
+      return m_app == nullptr;
     }
 
     /// get cache of object
-    auto get_cache() const noexcept
+    auto get_result() const noexcept
     {
-      assert(evaluated());
-      return m_cache;
+      assert(is_result());
+      return m_arg;
     }
 
     /// set cache of object
-    void set_cache(const object_ptr<const Object>& obj) const noexcept
+    void set_result(const object_ptr<const Object>& obj) const noexcept
     {
-      assert(!evaluated());
-      m_cache = obj;
-    }
-
-    /// clear cache
-    void clear_cache() const noexcept
-    {
-      m_cache = nullptr;
+      assert(!is_result());
+      m_app = nullptr;
+      m_arg = obj;
     }
 
   private:
-    /// cache
-    mutable object_ptr<const Object> m_cache;
     /// closure
     mutable object_ptr<const Object> m_app;
     /// argument

include/yave/rts/closure.hpp
@@ -1,0 +1,116 @@
+//
+// Copyright (c) 2019 mocabe (https://github.com/mocabe)
+// Distributed under LGPLv3 License. See LICENSE for more details.
+//
+
+#pragma once
+
+#include <yave/rts/box.hpp>
+#include <yave/rts/apply.hpp>
+
+namespace yave {
+
+  // fwd
+
+  template <uint64_t N>
+  struct ClosureN;
+  template <class Closure1 = ClosureN<1>>
+  struct Closure;
+
+  // ------------------------------------------
+  // closure_type_traits
+
+  /// Trait class for Object::obj_name for closure objects.
+  template <class T>
+  struct closure_type_traits
+  {
+    /// Object::obj_name can be used for ID of closures.
+    static constexpr const char name[] = "00000000-0000-0000-0000-000000000000";
+  };
+
+  // ------------------------------------------
+  // closure_info_table
+
+  /// Info table for closure
+  struct closure_info_table : object_info_table
+  {
+    /// Number of arguments
+    const uint64_t n_args;
+    /// vtable for code
+    object_ptr<const Object> (*code)(const Closure<>*) noexcept;
+  };
+
+  // ------------------------------------------
+  // Closure
+
+  /// Base class of Closue objects.
+  template <class Closure1>
+  struct Closure : Object
+  {
+    /// Arity of this closure.
+    mutable uint64_t arity;
+
+    /// Get number of args
+    auto n_args() const noexcept
+    {
+      return static_cast<const closure_info_table*>(info_table)->n_args;
+    }
+
+    /// PAP?
+    bool is_pap() const noexcept
+    {
+      return n_args() != arity;
+    }
+
+    /// get nth vertebrae
+    auto& vertebrae(uint64_t n) const noexcept
+    {
+      using arg_type = typename decltype(Closure1::spine)::value_type;
+      // offset to first element of argument buffer
+      constexpr uint64_t offset = offset_of_member(&Closure1::spine);
+      static_assert(offset % sizeof(arg_type) == 0);
+      // manually calc offset to avoid UB
+      return ((arg_type*)this)[offset / sizeof(arg_type) + n];
+    }
+
+    /// get nth argument
+    auto& arg(uint64_t n) const noexcept
+    {
+      return _get_storage(*vertebrae(n)).arg();
+    }
+
+  public: /* can modify mutable members */
+    /// Execute core with vtable function
+    auto call() const noexcept
+    {
+      return static_cast<const closure_info_table*>(info_table)->code(this);
+    }
+  };
+
+  /** \brief ClosureN
+   * Contains static size array for incoming arguments.
+   * Arguments will be filled from back to front.
+   * So, first argument of closure will be LAST element of array for arguments.
+   * If all arguments are passed, arity becomes zero and the closure is ready to
+   * execute code.
+   */
+  template <uint64_t N>
+  struct ClosureN : Closure<>
+  {
+    /// get raw arg
+    template <uint64_t Arg>
+    auto nth_arg() const noexcept
+    {
+      static_assert(Arg < N, "Invalid index of argument");
+      auto& app = spine[N - Arg - 1];
+
+      if (_get_storage(*app).is_result())
+        return _get_storage(*app).get_result();
+      else
+        return _get_storage(*app).arg();
+    }
+
+    /// args
+    mutable std::array<object_ptr<const Apply>, N> spine = {};
+  };
+}

include/yave/rts/eval.hpp
@@ -20,8 +20,8 @@ namespace yave {
     if (auto apply = value_cast_if<Apply>(obj)) {
       auto& apply_storage = _get_storage(*apply);
       // return cached value
-      if (apply_storage.evaluated()) {
-        return apply_storage.get_cache();
+      if (apply_storage.is_result()) {
+        return apply_storage.get_result();
       }
       // create new apply
       return make_object<Apply>(
@@ -31,85 +31,115 @@ namespace yave {
     return obj;
   }
 
-  /// clear apply cache
-  inline void clear_apply_cache(const object_ptr<const Object>& obj)
-  {
-    if (auto app = value_cast_if<Apply>(obj)) {
-      auto& storage = _get_storage(*app);
-      if (storage.evaluated())
-        storage.clear_cache();
-      clear_apply_cache(storage.app());
-      clear_apply_cache(storage.arg());
-    }
-  }
-
   namespace detail {
 
-    /// evaluate apply tree
-    [[nodiscard]] inline auto eval_obj(const object_ptr<const Object>& obj)
-      -> object_ptr<const Object>
+    [[nodiscard]] inline auto build_spine_stack(
+      object_ptr<const Object> obj,
+      std::vector<object_ptr<const Apply>>& stack) -> object_ptr<const Object>
     {
-      // detect exception
-      if (unlikely(has_exception_tag(obj)))
-        throw result_error::exception_result(
-          clear_pointer_tag(get_tagged_exception(obj)));
+      // we, hopeless C++ programmers, manually optimize tailcalls because
+      // compilers sometimes not smart enough to do that for us.
 
-      // apply
-      if (auto apply = value_cast_if<Apply>(obj)) {
+      auto next = std::move(obj);
 
-        // alias: internal storage
-        auto& apply_storage = _get_storage(*apply);
+      for (;;) {
 
-        // graph reduction
-        if (apply_storage.evaluated()) {
-          return apply_storage.get_cache();
-        }
+        if (auto apply = value_cast_if<Apply>(next)) {
 
-        // whnf
-        auto app = eval_obj(apply_storage.app());
+          auto& apply_storage = _get_storage(*apply);
 
-        // alias: argument
-        const auto& arg = apply_storage.arg();
-        // alias: app closure
-        auto capp = static_cast<const Closure<>*>(app.get());
-
-        /*
-          These exceptions should not triggered on well-typed input. Just
-          leaving it here to avoid catastrophic heap corruption when something
-          went totally wrong.
-        */
-        if (unlikely(!has_arrow_type(app))) {
-          throw eval_error::bad_apply();
-        }
-        if (unlikely(capp->arity == 0)) {
-          throw eval_error::too_many_arguments();
-        }
-
-        // clone closure and apply
-        auto ret = [&] {
-          // clone
-          auto pap = clone(app);
-          // alias: pap closure
-          auto cpap = static_cast<const Closure<>*>(pap.get());
-
-          // push argument
-          auto arity       = --cpap->arity;
-          cpap->arg(arity) = arg;
-
-          // call code()
-          if (unlikely(arity == 0)) {
-            return eval_obj(cpap->code());
+          // graph reduction
+          if (apply_storage.is_result()) {
+            // assume Exception is not cached
+            next = apply_storage.get_result();
+            continue;
           }
 
-          return pap;
-        }();
+          if (unlikely(stack.empty()))
+            stack.reserve(8);
 
-        // set cache
-        apply_storage.set_cache(ret);
+          // push vertebrae
+          stack.push_back(apply);
 
-        return ret;
+          next = apply_storage.app();
+          continue;
+        }
+        // bottom
+        return next;
       }
-      return obj;
+    };
+
+    [[nodiscard]] inline auto eval_spine(
+      object_ptr<const Object> obj,
+      std::vector<object_ptr<const Apply>>& stack) -> object_ptr<const Object>
+    {
+      auto next = std::move(obj);
+
+      for (;;) {
+
+        // detect exception
+        if (unlikely(has_exception_tag(next)))
+          throw result_error::exception_result(
+            clear_pointer_tag(get_tagged_exception(next)));
+
+        if (auto apply = value_cast_if<Apply>(next)) {
+
+          auto& apply_storage = _get_storage(*apply);
+
+          // when we don't have any arguments to apply in stack and apply node
+          // has already evaluated, we can directly return cached result. if we
+          // can guarantee cached results are only PAP or values, we actually
+          // don't need to call eval_spine() on these results.
+          if (unlikely(stack.empty() && apply_storage.is_result()))
+            return apply_storage.get_result();
+
+          // build stack and get bottom closure
+          auto bottom = build_spine_stack(std::move(next), stack);
+
+          assert(has_arrow_type(bottom));
+
+          // clone bottom closure
+          auto fun   = clone(bottom);
+          auto cfun  = reinterpret_cast<const Closure<>*>(fun.get());
+          auto arity = cfun->arity;
+
+          // when stack size is not enough, return PAP
+          if (unlikely(stack.size() < arity)) {
+            for (size_t i = 0; i < stack.size(); ++i) {
+              cfun->vertebrae(arity - stack.size() + i) = std::move(stack[i]);
+            }
+            cfun->arity -= stack.size();
+            return fun;
+          }
+
+          // base of stack
+          auto base_iter = stack.end() - arity;
+
+          // dump stack into closure
+          for (size_t i = 0; i < arity; ++i) {
+            cfun->vertebrae(i) = std::move(*(base_iter + i));
+          }
+          cfun->arity = 0;
+
+          // call code
+          auto result = cfun->call();
+
+          // unwind stack consumed
+          stack.erase(base_iter, stack.end());
+
+          next = std::move(result);
+          continue;
+        }
+        return next;
+      }
+    }
+
+    /// evaluate apply tree
+    [[nodiscard]] inline auto eval_obj(object_ptr<const Object> obj)
+      -> object_ptr<const Object>
+    {
+      std::vector<object_ptr<const Apply>> stack;
+      return eval_spine(std::move(obj), stack);
     }
   } // namespace detail
 
@@ -134,7 +164,7 @@ namespace yave {
       using To =
         std::add_const_t<typename decltype(guess_object_type(type))::type>;
       // cast to resutn type
-      return static_object_cast<To>(result);
+      return static_object_cast<To>(std::move(result));
     } else {
       // fallback to object_ptr<>
       return result;

include/yave/rts/fix.hpp
@@ -14,8 +14,9 @@ namespace yave {
 
   namespace detail {
     class Fix_X;
-  }
+  } // namespace detail
 
+  /// Implementation of Y using self referencing apply node.
   // fix: (a -> a) -> a.
   // fix f = let x = f x in x
   struct Fix
@@ -25,28 +26,33 @@ namespace yave {
     {
       // reduce argument closure
       object_ptr<const Object> f = eval_arg<0>();
+      auto cf                    = reinterpret_cast<const Closure<>*>(f.get());
+
+      assert(has_arrow_type(f));
 
       // check arity for safety
-      auto c = static_cast<const Closure<>*>(_get_storage(f).get());
-      if (unlikely(c->arity == 0)) {
+      if (unlikely(cf->arity == 0)) {
         throw eval_error::bad_fix();
       }
 
       auto ret = [&] {
         // create return value
-        auto pap = clone(f);
-        auto cc  = static_cast<const Closure<>*>(_get_storage(pap).get());
+        auto pap   = clone(f);
+        auto cpap  = reinterpret_cast<const Closure<>*>(pap.get());
+        auto cthis = reinterpret_cast<const Closure<>*>(this);
+
+        auto& app = cthis->vertebrae(0);
 
         // build self-referencing closure
-        auto arity     = --cc->arity;
-        cc->arg(arity) = pap;
+        auto arity             = --cpap->arity;
+        cpap->vertebrae(arity) = app;
 
         // avoid memory leak
-        cc->refcount.fetch_sub();
+        _get_storage(app).decrement_refcount();
 
         // eval
         if (unlikely(arity == 0))
-          return detail::eval_obj(cc->code());
+          return cpap->call();
 
         return pap;
       }();
@@ -55,6 +61,17 @@ namespace yave {
       return static_object_cast<const VarValueProxy<detail::Fix_X>>(
         std::move(ret));
     }
-  };
 
+    auto _self_update(object_ptr<const Object> result) const noexcept
+      -> object_ptr<const Object>
+    {
+      // set cache
+      auto cthis = reinterpret_cast<const Closure<>*>(this);
+      auto& app  = cthis->vertebrae(0);
+      _get_storage(*app).set_result(std::move(result));
+
+      // return @
+      return app;
+    }
+  };
 } // namespace yave

include/yave/rts/function.hpp
@@ -5,7 +5,7 @@
 
 #pragma once
 
-#include <yave/support/offset_of_member.hpp>
+#include <yave/rts/closure.hpp>
 
 #include <yave/rts/static_typing.hpp>
 #include <yave/rts/dynamic_typing.hpp>
@@ -18,102 +18,37 @@
 #include <yave/rts/value_cast.hpp>
 
 #include <yave/obj/string/string.hpp>
+#include <yave/support/offset_of_member.hpp>
 
 namespace yave {
 
-  // ------------------------------------------
-  // Closure
-
-  // forward decl
-  template <uint64_t N>
-  struct ClosureN;
-
-  template <class Closure1 = ClosureN<1>>
-  struct Closure;
-
-  /// Info table for closure
-  struct closure_info_table : object_info_table
-  {
-    /// Number of arguments
-    const uint64_t n_args;
-    /// vtable for code
-    object_ptr<const Object> (*code)(const Closure<>*) noexcept;
-  };
-
-  /// Trait class for Object::obj_name for closure objects.
-  template <class T>
-  struct closure_type_traits
-  {
-    /// Object::obj_name can be used for ID of closures.
-    static constexpr const char name[] = "00000000-0000-0000-0000-000000000000";
-  };
-
-  template <class Closure1>
-  struct Closure : Object
-  {
-    /// Arity of this closure.
-    mutable uint64_t arity;
-
-    /// Get number of args
-    auto n_args() const noexcept
-    {
-      return static_cast<const closure_info_table*>(info_table)->n_args;
-    }
-
-    /// Execute core with vtable function
-    auto code() const noexcept
-    {
-      return static_cast<const closure_info_table*>(info_table)->code(this);
-    }
-
-    /// get nth argument
-    auto& arg(uint64_t n) const noexcept
-    {
-      using arg_type = typename decltype(Closure1::args)::value_type;
-      // offset to first element of argument buffer
-      constexpr uint64_t offset = offset_of_member(&Closure1::args);
-      static_assert(offset % sizeof(arg_type) == 0);
-      // manually calc offset to avoid UB
-      return ((arg_type*)this)[offset / sizeof(arg_type) + n];
-    }
-  };
-
-  /** \brief ClosureN
-   * Contains static size array for incoming arguments.
-   * Arguments will be filled from back to front.
-   * So, first argument of closure will be LAST element of array for arguments.
-   * If all arguments are passed, arity becomes zero and the closure is ready to
-   * execute code.
-   */
-  template <uint64_t N>
-  struct ClosureN : Closure<>
-  {
-    /// get raw arg
-    template <uint64_t Arg>
-    auto& nth_arg() const noexcept
-    {
-      static_assert(Arg < N, "Invalid index of argument");
-      return args[N - Arg - 1];
-    }
-
-    /// args
-    mutable std::array<object_ptr<const Object>, N> args = {};
-  };
+  namespace detail {
+    // fwd
+    inline auto eval_obj(object_ptr<const Object> obj)
+      -> object_ptr<const Object>;
+  } // namespace detail
 
   // ------------------------------------------
   // vtbl_code_func
 
   /// vrtable function to call code()
   template <class T>
-  object_ptr<const Object> vtbl_code_func(const Closure<>* _this) noexcept
+  object_ptr<const Object> vtbl_code_func(const Closure<>* _cthis) noexcept
   {
-    auto ret = [&]() -> object_ptr<const Object> {
-      try {
-        auto r = (static_cast<const T*>(_this)->exception_handler()).value();
-        assert(r);
-        return r;
+    auto _this = static_cast<const T*>(_cthis);
 
-        // bad_value_cast
+    auto ret = [&]() -> object_ptr<const Object> {
+
+      try {
+
+        // calls code() inside.
+        auto r = _this->_handle_exception().value();
+        assert(r);
+
+        // only cache PAP or values (see comments in eval_spine()).
+        return detail::eval_obj(std::move(r));
+
+        // type_error
       } catch (const bad_value_cast& e) {
         return add_exception_tag(to_Exception(e));
 
@@ -161,7 +96,8 @@ namespace yave {
     if (!has_exception_tag(ret))
       assert(!value_cast_if<Exception>(ret));
 
-    return ret;
+    // call self update method
+    return _this->_self_update(std::move(ret));
   }
 
   // ------------------------------------------
@@ -304,7 +240,7 @@ namespace yave {
                                          &info_table_initializer::info_table)},
                                        other.arity,
                                      },
-                                     other.args}
+                                     other.spine}
     {
     }
 
@@ -315,7 +251,7 @@ namespace yave {
                                          &info_table_initializer::info_table)},
                                        std::move(other.arity),
                                      },
-                                     std::move(other.args)}
+                                     std::move(other.spine)}
     {
     }
 
@@ -323,7 +259,7 @@ namespace yave {
     Function& operator=(const Function& other) noexcept
     {
       arity = other.arity;
-      args  = other.args;
+      spine = other.spine;
       return *this;
     }
 
@@ -331,7 +267,7 @@ namespace yave {
     Function& operator=(Function&& other) noexcept
     {
       arity = std::move(other.arity);
-      args  = std::move(other.args);
+      spine = std::move(other.spine);
       return *this;
     }
 
@@ -358,7 +294,7 @@ namespace yave {
       static_assert(std::is_standard_layout_v<To>);
       auto obj = ClosureN<sizeof...(Ts) - 1>::template nth_arg<N>();
       assert(obj);
-      return static_object_cast<To>(obj);
+      return static_object_cast<To>(std::move(obj));
     }
 
     /// evaluate N'th argument and take result
@@ -369,11 +305,20 @@ namespace yave {
       return eval(this->template arg<N>());
     }
 
-  public:
-    /// default exception handler
-    exception_handler_return_type exception_handler() const
+  public: /* customization points */
+    /// default exception handler.
+    auto _handle_exception() const -> exception_handler_return_type
     {
       return static_cast<const T*>(this)->code();
+    }
+
+    /// default self-update function.
+    auto _self_update(object_ptr<const Object> result) const noexcept
+      -> object_ptr<const Object>
+    {
+      auto cthis = reinterpret_cast<const Closure<>*>(this);
+      _get_storage(*cthis->vertebrae(0)).set_result(result);
+      return result;
     }
 
   private:
@@ -381,9 +326,11 @@ namespace yave {
     using base = ClosureN<sizeof...(Ts) - 1>;
     using base::arity;
     using base::n_args;
-    using base::code;
+    using base::call;
+    using base::vertebrae;
+    using base::is_pap;
     using base::arg;
-    using base::args;
+    using base::spine;
     using base::nth_arg;
 
     /// check signature of code()

test/rts/eval.cpp
@@ -22,6 +22,15 @@ YAVE_DECL_TYPE(Bool, "7ba340e7-8c41-41bc-a1f9-bea2a2db077d");
 
 TEST_CASE("eval")
 {
+  SECTION("value")
+  {
+    SECTION("i")
+    {
+      auto i = make_object<Int>();
+      REQUIRE(i == eval(i));
+    }
+  }
+
   SECTION("smpl")
   {
     struct F : Function<F, Int, Int, Int>
@@ -34,9 +43,16 @@ TEST_CASE("eval")
 
     auto f = make_object<F>();
 
+    SECTION("0")
+    {
+      REQUIRE(object_ptr<const Object>(f) == object_ptr<const Object>(eval(f)));
+    }
+
     SECTION("1")
     {
       auto term = f << new Int(42);
+      REQUIRE(type_of(term));
+      (void)eval(term);
       REQUIRE(type_of(term));
     }
 
@@ -45,12 +61,14 @@ TEST_CASE("eval")
       auto term = f << new Int(42) << new Int(24);
       REQUIRE(type_of(term));
       REQUIRE(*value_cast<Int>(eval(term)) == 66);
+      REQUIRE(*value_cast<Int>(eval(term)) == 66);
     }
 
     SECTION("3")
     {
       auto term = f << new Int(42) << (f << new Int(24) << new Int(24));
       REQUIRE(type_of(term));
+      REQUIRE(*value_cast<Int>(eval(term)) == 90);
       REQUIRE(*value_cast<Int>(eval(term)) == 90);
     }
 
@@ -59,6 +77,27 @@ TEST_CASE("eval")
       auto term = f << (f << new Int(42) << new Int(24))
                     << (f << new Int(42) << new Int(24));
       REQUIRE(type_of(term));
+      REQUIRE(*value_cast<Int>(eval(term)) == 132);
+      REQUIRE(*value_cast<Int>(eval(term)) == 132);
+    }
+
+    SECTION("5")
+    {
+      auto part = eval(f << new Int(42));
+      auto term = part << new Int(24);
+      REQUIRE(type_of(term));
+      REQUIRE(*value_cast<Int>(eval(term)) == 66);
+      REQUIRE(*value_cast<Int>(eval(term)) == 66);
+    }
+
+    SECTION("6")
+    {
+      auto app = f << new Int(42) << new Int(24);
+      (void)eval(app); // result will be cached
+      auto term = f << app << app;
+      REQUIRE(type_of(app));
+      REQUIRE(type_of(term));
+      REQUIRE(*value_cast<Int>(eval(term)) == 132);
       REQUIRE(*value_cast<Int>(eval(term)) == 132);
     }
   }
@@ -99,6 +138,43 @@ TEST_CASE("eval")
     }
   }
 
+  SECTION("ho2")
+  {
+    struct F : Function<F, closure<Int, Int, Int>, Int, closure<Int, Int>>
+    {
+      return_type code() const
+      {
+        return arg<0>() << arg<1>();
+      }
+    };
+
+    struct G : Function<G, Int, Int, Int>
+    {
+      return_type code() const
+      {
+        return arg<1>();
+      }
+    };
+
+    auto f = make_object<F>();
+    auto g = make_object<G>();
+
+    SECTION("0")
+    {
+      auto part = f << g << new Int(2019);
+      (void)eval(part);
+
+      auto t1 = part << new Int(42);
+      auto t2 = part << new Int(24);
+
+      REQUIRE(type_of(t1));
+      REQUIRE(type_of(t2));
+
+      REQUIRE(*value_cast<Int>(eval(t1)) == 42);
+      REQUIRE(*value_cast<Int>(eval(t2)) == 24);
+    }
+  }
+
   SECTION("rec")
   {
     struct Fib : Function<Fib, Int, Int>
@@ -121,9 +197,7 @@ TEST_CASE("eval")
 
       return_type code() const
       {
-        static const auto fix  = make_object<Fix>();
-        static const auto impl = make_object<Impl>();
-        return fix << impl << arg<0>();
+        return make_object<Fix>() << make_object<Impl>() << arg<0>();
       }
     };
 
@@ -132,6 +206,8 @@ TEST_CASE("eval")
     SECTION("0")
     {
       auto term = fib << new Int(10);
+      REQUIRE(type_of(term));
+      REQUIRE(*value_cast<Int>(eval(term)) == 55);
       REQUIRE(type_of(term));
       REQUIRE(*value_cast<Int>(eval(term)) == 55);
     }

commit ce540384f64554e2530a5e6f9ad06f17deb68bc9

rts: Fix incorrect vartebrae index for root of PAP
regression from (#10)...

include/yave/rts/dynamic_typing.hpp
@@ -510,7 +510,8 @@ namespace yave {
         auto c = reinterpret_cast<const Closure<>*>(obj.get());
         // pap: return root apply node
         // app: get_type
-        return c->is_pap() ? type_of_impl(c->vertebrae(0)) : get_type(obj);
+        return c->is_pap() ? type_of_impl(c->vertebrae(c->arity))
+                           : get_type(obj);
       }
 
       // value -> value