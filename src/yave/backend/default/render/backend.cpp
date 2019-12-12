a3becc57d0e84ddf865325e0e3dd21770e0fae8b
mocabe <hisa.mocabe@gmail.com>
2019-12-12 17:25:25 +0900

    backend: Add missing decl and defs

--- src/yave/backend/default/render/backend.cpp
+++ src/yave/backend/default/render/backend.cpp
@@ -66,7 +66,7 @@ namespace yave::backends::default_render {
 
   void backend::update(const scene_config& config)
   {
-    return;
+    m_pimpl->config = config;
   }
 
   bool backend::initialized() const
@@ -79,6 +79,19 @@ namespace yave::backends::default_render {
     std::vector<node_declaration> ret;
 
     {
+      ret.push_back(get_node_declaration<node::BlendOpSrc>());
+      ret.push_back(get_node_declaration<node::BlendOpDst>());
+      ret.push_back(get_node_declaration<node::BlendOpOver>());
+      ret.push_back(get_node_declaration<node::BlendOpIn>());
+      ret.push_back(get_node_declaration<node::BlendOpOut>());
+      ret.push_back(get_node_declaration<node::BlendOpAdd>());
+      ret.push_back(get_node_declaration<node::LayerCompositor>());
+      ret.push_back(get_node_declaration<node::FrameBuffer>());
+      ret.push_back(get_node_declaration<node::LoadImage>());
+      ret.push_back(get_node_declaration<node::LayerImageOutput>());
+      ret.push_back(get_node_declaration<node::KeyframeEvaluatorInt>());
+      ret.push_back(get_node_declaration<node::KeyframeEvaluatorFloat>());
+      ret.push_back(get_node_declaration<node::KeyframeEvaluatorBool>());
     }
 
     return ret;
@@ -95,6 +108,19 @@ namespace yave::backends::default_render {
 
     { // clang-format off
 
+      def(yave::get_node_definitions<node::BlendOpSrc, backend_tags::default_render>());
+      def(yave::get_node_definitions<node::BlendOpDst, backend_tags::default_render>());
+      def(yave::get_node_definitions<node::BlendOpOver, backend_tags::default_render>());
+      def(yave::get_node_definitions<node::BlendOpIn, backend_tags::default_render>());
+      def(yave::get_node_definitions<node::BlendOpOut, backend_tags::default_render>());
+      def(yave::get_node_definitions<node::BlendOpAdd, backend_tags::default_render>());
+      def(yave::get_node_definitions<node::LayerCompositor, backend_tags::default_render>());
+      def(yave::get_node_definitions<node::FrameBuffer, backend_tags::default_render>(&(*m_pimpl->fb_manager)));
+      def(yave::get_node_definitions<node::LoadImage, backend_tags::default_render>());
+      def(yave::get_node_definitions<node::LayerImageOutput, backend_tags::default_render>());
+      def(yave::get_node_definitions<node::KeyframeEvaluatorInt, backend_tags::default_render>());
+      def(yave::get_node_definitions<node::KeyframeEvaluatorFloat, backend_tags::default_render>());
+      def(yave::get_node_definitions<node::KeyframeEvaluatorBool, backend_tags::default_render>());
 
     } // clang-format on
 
