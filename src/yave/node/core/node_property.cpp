662ec8f2a65c73434873170f1fe008d046d12fea
mocabe <hisa.mocabe@gmail.com>
2019-12-24 00:12:40 +0900

    node_property: Remove primitive related stuff

--- src/yave/node/core/node_property.cpp
+++ src/yave/node/core/node_property.cpp
@@ -8,52 +8,21 @@
 
 namespace yave {
 
-  node_property::node_property(normal_construct_t, const std::string& name)
+  node_property::node_property(const std::string& name, node_type type)
     : m_name {name}
-    , m_type {node_type::normal}
-    , m_prim {std::nullopt}
-    , m_visited {0}
+    , m_type {type}
+    , m_visited {false}
   {
   }
 
-  node_property::node_property(
-    primitive_construct_t,
-    const std::string& name,
-    const primitive_t& prim)
-    : m_name {name}
-    , m_type {node_type::primitive}
-    , m_prim {make_object<PrimitiveContainer>(prim)}
-    , m_visited {0}
+  auto node_property::name() const -> const std::string&
   {
-  }
-
-  node_property::node_property(interface_construct_t, const std::string& name)
-    : m_name {name}
-    , m_type {node_type::interface}
-    , m_prim {std::nullopt}
-    , m_visited {0}
-  {
-  }
-
-  node_property::node_property(const node_property& other)
-    : m_name {other.m_name}
-    , m_type {other.m_type}
-    , m_prim {other.m_prim}
-    , m_visited {0}
-  {
-    // clone prim container
-    if (m_prim)
-      *m_prim = clone(*m_prim);
+    return m_name;
   }
 
   bool node_property::is_normal() const
   {
     return m_type == node_type::normal;
-  }
-
-  bool node_property::is_primitive() const
-  {
-    return m_type == node_type::primitive;
   }
 
   bool node_property::is_interface() const
@@ -66,35 +35,9 @@ namespace yave {
     return m_type;
   }
 
-  auto node_property::name() const -> const std::string&
-  {
-    return m_name;
-  }
-
   void node_property::set_name(const std::string& new_name)
   {
     m_name = new_name;
-  }
-
-  auto node_property::get_primitive() const -> primitive_t
-  {
-    assert(is_primitive());
-    return m_prim.value()->get();
-  }
-
-  void node_property::set_primitive(const primitive_t& prim)
-  {
-    if (is_primitive())
-      m_prim.value()->set(prim);
-  }
-
-  auto node_property::get_shared_primitive() const
-    -> object_ptr<PrimitiveContainer>
-  {
-    if (is_primitive())
-      return m_prim.value();
-    else
-      return nullptr;
   }
 
   bool node_property::is_visited() const
