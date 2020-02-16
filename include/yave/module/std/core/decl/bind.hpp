d2e39fe9afa7c91cbfa99eea05bac28361959eef
mocabe <hisa.mocabe@gmail.com>
2020-02-16 16:03:02 +0900

    Add bind nodes

---
+++ include/yave/module/std/core/decl/bind.hpp
@@ -0,0 +1,26 @@
+//
+// Copyright (c) 2019 mocabe (https://github.com/mocabe)
+// Distributed under LGPLv3 License. See LICENSE for more details.
+//
+
+#pragma once
+
+#include <yave/node/core/get_info.hpp>
+
+namespace yave {
+
+  namespace node {
+    /// Bind
+    struct Bind;
+  } // namespace node
+
+  template <>
+  struct node_declaration_traits<node::Bind>
+  {
+    static auto get_node_declaration() -> node_declaration
+    {
+      return node_declaration(
+        "Bind", {"fn", "arg"}, {"fn"}, "Apply argument to function");
+    }
+  };
+} // namespace yave
