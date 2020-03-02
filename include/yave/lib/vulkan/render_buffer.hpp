a396f76101989fb11ed8e8e4c18fe333526b0823
mocabe <hisa.mocabe@gmail.com>
2020-03-02 19:26:03 +0900

    Implement basic rendering

---
+++ include/yave/lib/vulkan/render_buffer.hpp
@@ -0,0 +1,50 @@
+//
+// Copyright (c) 2019 mocabe (https://github.com/mocabe)
+// Distributed under LGPLv3 License. See LICENSE for more details.
+//
+
+#pragma once
+
+#include <yave/config/config.hpp>
+#include <yave/lib/vulkan/vulkan_util.hpp>
+
+namespace yave::vulkan {
+
+  /// Buffer data for rendering
+  struct render_buffer
+  {
+    /// buffer usage e.g. Vertex or Fragment
+    vk::BufferUsageFlags usage;
+    /// current size of buffer
+    vk::DeviceSize size;
+    /// total capacity of buffer
+    vk::DeviceSize capacity;
+    /// buffer
+    vk::UniqueBuffer buffer;
+    /// buffer memory
+    vk::UniqueDeviceMemory memory;
+  };
+
+  /// Create new render buffer
+  [[nodiscard]] auto create_render_buffer(
+    const vk::BufferUsageFlagBits& usg,
+    const vk::DeviceSize& size,
+    const vk::Device& device,
+    const vk::PhysicalDevice& physicalDevice) -> render_buffer;
+
+  /// Resize render buffer
+  void resize_render_buffer(
+    render_buffer& buff,
+    const vk::DeviceSize& newSize,
+    const vk::Device& device,
+    const vk::PhysicalDevice& physicalDevice);
+
+  /// Upload data to GPU
+  void store_render_buffer(
+    render_buffer& dst,
+    const std::byte* srcData,
+    const vk::DeviceSize& srcOffset,
+    const vk::DeviceSize& srcSize,
+    const vk::Device& device,
+    const vk::PhysicalDevice& physicalDevice);
+}
---
+++ src/yave/lib/vulkan/render_buffer.cpp
@@ -0,0 +1,112 @@
+//
+// Copyright (c) 2019 mocabe (https://github.com/mocabe)
+// Distributed under LGPLv3 License. See LICENSE for more details.
+//
+
+#include <yave/lib/vulkan/render_buffer.hpp>
+
+namespace yave::vulkan {
+
+  auto create_render_buffer(
+    const vk::BufferUsageFlagBits& usg,
+    const vk::DeviceSize& size,
+    const vk::Device& device,
+    const vk::PhysicalDevice& physicalDevice) -> render_buffer
+  {
+    // Vulkan cannot create zero sized buffer!
+    assert(size != 0);
+
+    vk::UniqueBuffer buffer;
+    {
+      vk::BufferCreateInfo info;
+      info.size        = size;
+      info.usage       = usg;
+      info.sharingMode = vk::SharingMode::eExclusive;
+
+      buffer = device.createBufferUnique(info);
+    }
+
+    vk::UniqueDeviceMemory memory;
+    {
+      auto req = device.getBufferMemoryRequirements(buffer.get());
+
+      vk::MemoryAllocateInfo info;
+      info.allocationSize  = req.size;
+      info.memoryTypeIndex = find_memory_type_index(
+        req, vk::MemoryPropertyFlagBits::eHostVisible, physicalDevice);
+
+      memory = device.allocateMemoryUnique(info);
+      device.bindBufferMemory(buffer.get(), memory.get(), 0);
+    }
+
+    return {usg, size, size, std::move(buffer), std::move(memory)};
+  }
+
+  void resize_render_buffer(
+    render_buffer& buff,
+    const vk::DeviceSize& newSize,
+    const vk::Device& device,
+    const vk::PhysicalDevice& physicalDevice)
+  {
+    assert(newSize != 0);
+
+    if (buff.usage == vk::BufferUsageFlags())
+      throw std::runtime_error("Render buffer is not initialized");
+
+    // new buffer
+    vk::UniqueBuffer buffer;
+    {
+      vk::BufferCreateInfo info;
+      info.size        = newSize;
+      info.usage       = buff.usage;
+      info.sharingMode = vk::SharingMode::eExclusive;
+
+      buffer = device.createBufferUnique(info);
+    }
+
+    // reuse memory when in capacity
+    if (newSize <= buff.capacity) {
+      device.bindBufferMemory(buffer.get(), buff.memory.get(), 0);
+      buff.buffer = std::move(buffer);
+      buff.size   = newSize;
+      return;
+    }
+
+    vk::UniqueDeviceMemory memory;
+    {
+      auto req = device.getBufferMemoryRequirements(buffer.get());
+
+      vk::MemoryAllocateInfo info;
+      info.allocationSize  = req.size;
+      info.memoryTypeIndex = find_memory_type_index(
+        req, vk::MemoryPropertyFlagBits::eHostVisible, physicalDevice);
+
+      memory = device.allocateMemoryUnique(info);
+    }
+
+    buff.size     = newSize;
+    buff.capacity = newSize;
+    buff.memory   = std::move(memory);
+    buff.buffer   = std::move(buffer);
+  }
+
+  void store_render_buffer(
+    render_buffer& dst,
+    const std::byte* srcData,
+    const vk::DeviceSize& srcOffset,
+    const vk::DeviceSize& srcSize,
+    const vk::Device& device,
+    const vk::PhysicalDevice& physicalDevice)
+  {
+    assert(dst.size > srcOffset + srcSize);
+    void* ptr = device.mapMemory(dst.memory.get(), srcOffset, srcSize);
+    std::memcpy(ptr, srcData, srcSize);
+
+    // flush since buffers are non-coherent
+    vk::MappedMemoryRange range;
+    range.memory = dst.memory.get();
+    device.flushMappedMemoryRanges(range);
+
+    device.unmapMemory(dst.memory.get());
+  }
+}
