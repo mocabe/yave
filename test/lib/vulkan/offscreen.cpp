eebab905c49343115ff7004ae30e9a7801b70c83
mocabe <hisa.mocabe@gmail.com>
2020-03-04 17:41:12 +0900

    Add some tests

--- test/lib/vulkan/offscreen.cpp
+++ test/lib/vulkan/offscreen.cpp
@@ -6,13 +6,16 @@
 #include <yave/lib/vulkan/offscreen_render_pass.hpp>
 #include <yave/lib/vulkan/offscreen_renderer_2D.hpp>
 #include <yave/lib/glfw/glfw_context.hpp>
+#include <yave/lib/vulkan/image.hpp>
 #include <yave/lib/image/image.hpp>
-
+#include <boost/dll/runtime_symbol_info.hpp>
+#include <boost/gil/extension/io/bmp.hpp>
 #include <catch2/catch.hpp>
 
 #include <iostream>
 
 using namespace yave;
+using namespace vulkan;
 
 TEST_CASE("rgba32f compos pass")
 {
@@ -54,10 +57,36 @@ TEST_CASE("rgba32f compos pass")
 TEST_CASE("rgba32f compos 2D")
 {
   glfw::glfw_context glfw {};
-  vulkan::vulkan_context ctx {glfw};
-  vulkan::rgba32f_offscreen_renderer_2D pipeline(1920, 1080, ctx);
+  vulkan_context ctx {glfw};
+  rgba32f_offscreen_renderer_2D renderer(1920, 1080, ctx);
 
-  boost::gil::rgba32f_image_t img {160, 90};
-  auto tex = pipeline.add_texture(const_view(img));
-  pipeline.remove_texture(tex);
+  draw2d_data data;
+  draw2d_list list;
+  list.idx_buffer = {{0}, {1}, {2}};
+  list.vtx_buffer = {{{0, 0}, {}, {1, 0, 0, 1}},
+                     {{1920, 540}, {}, {0, 1, 0, 1}},
+                     {{960, 1080}, {}, {0, 0, 1, 1}}};
+  list.cmd_buffer = {{3, 0, 0, {}, {{0, 0}, {1920, 1080}}}};
+  data.draw_lists.push_back(list);
+  data.draw_lists.push_back(list);
+
+  auto start = std::chrono::high_resolution_clock::now();
+
+  for (auto i = 0; i < 100; ++i) 
+    renderer.render(data);
+
+  auto dur = std::chrono::duration_cast<std::chrono::milliseconds>(
+    std::chrono::high_resolution_clock::now() - start);
+  std::cout << "Time elapsed: " << dur.count() << std::endl;
+
+  boost::gil::rgba32f_image_t img(renderer.width(), renderer.height(), 0);
+  renderer.load_frame(view(img));
+
+  boost::gil::rgb8_image_t out_image(renderer.width(), renderer.height());
+  boost::gil::copy_and_convert_pixels(view(img), view(out_image));
+
+  auto path =
+    boost::dll::program_location().remove_filename() / "out_image.bmp";
+
+  boost::gil::write_view(path.string(), view(out_image), boost::gil::bmp_tag());
 }
