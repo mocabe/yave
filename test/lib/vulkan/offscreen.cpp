3a7643212a4df8d3f11b0f638c1829a61192214d
mocabe <hisa.mocabe@gmail.com>
2020-03-07 21:53:46 +0900

    [test][vulkan] Add some tests

--- test/lib/vulkan/offscreen.cpp
+++ test/lib/vulkan/offscreen.cpp
@@ -58,35 +58,84 @@ TEST_CASE("rgba32f compos 2D")
 {
   glfw::glfw_context glfw {};
   vulkan_context ctx {glfw};
-  rgba32f_offscreen_renderer_2D renderer(1920, 1080, ctx);
+  uint32_t width  = 1280;
+  uint32_t height = 720;
+  rgba32f_offscreen_renderer_2D renderer(width, height, ctx);
 
-  draw2d_data data;
-  draw2d_list list;
-  list.idx_buffer = {{0}, {1}, {2}};
-  list.vtx_buffer = {{{0, 0}, {}, {1, 0, 0, 1}},
-                     {{1920, 540}, {}, {0, 1, 0, 1}},
-                     {{960, 1080}, {}, {0, 0, 1, 1}}};
-  list.cmd_buffer = {{3, 0, 0, {}, {{0, 0}, {1920, 1080}}}};
-  data.draw_lists.push_back(list);
-  data.draw_lists.push_back(list);
+  SECTION("triangle")
+  {
+    draw2d_data data;
+    draw2d_list list;
+    list.idx_buffer = {{0}, {1}, {2}};
+    list.vtx_buffer = {{{0, 0}, {}, {1, 0, 0, 1}},
+                       {{width, height / 2}, {}, {0, 1, 0, 1}},
+                       {{width / 2, height}, {}, {0, 0, 1, 1}}};
+    list.cmd_buffer = {
+      {3, 0, 0, {}, {{0, 0}, {renderer.width(), renderer.height()}}}};
+    data.draw_lists.push_back(list);
+    data.draw_lists.push_back(list);
 
-  auto start = std::chrono::high_resolution_clock::now();
+    auto start = std::chrono::high_resolution_clock::now();
 
-  for (auto i = 0; i < 100; ++i) 
+    for (auto i = 0; i < 100; ++i)
+      renderer.render(data);
+
+    auto dur = std::chrono::duration_cast<std::chrono::milliseconds>(
+      std::chrono::high_resolution_clock::now() - start);
+    std::cout << "Time elapsed: " << dur.count() << std::endl;
+
+    boost::gil::rgba32f_image_t img(renderer.width(), renderer.height(), 0);
+    renderer.load_frame(view(img));
+
+    boost::gil::rgb8_image_t out_image(renderer.width(), renderer.height());
+    boost::gil::copy_and_convert_pixels(view(img), view(out_image));
+
+    auto path =
+      boost::dll::program_location().remove_filename() / "triangle.bmp";
+
+    boost::gil::write_view(
+      path.string(), view(out_image), boost::gil::bmp_tag());
+  }
+
+  SECTION("rect")
+  {
+    draw2d_data data;
+    draw2d_list list;
+
+    uint32_t d                = 50;
+    std::vector<glm::vec2> ps = {
+      {d, d}, {width - d, d}, {width - d, height - d}, {d, height - d}};
+
+    list.add_polygon(
+      ps.data(),
+      ps.size(),
+      {0, 0, 1, 1},
+      polygon_flags(),
+      {{0, 0}, {width, height}},
+      renderer.default_texture());
+
+    list.add_polyline(
+      ps.data(),
+      ps.size(),
+      {0, 1, 0, 1},
+      10,
+      polyline_flags::closed,
+      {{0, 0}, {width, height}},
+      renderer.default_texture());
+
+    data.draw_lists.push_back(std::move(list));
+
     renderer.render(data);
 
-  auto dur = std::chrono::duration_cast<std::chrono::milliseconds>(
-    std::chrono::high_resolution_clock::now() - start);
-  std::cout << "Time elapsed: " << dur.count() << std::endl;
+    boost::gil::rgba32f_image_t img(renderer.width(), renderer.height(), 0);
+    renderer.load_frame(view(img));
 
-  boost::gil::rgba32f_image_t img(renderer.width(), renderer.height(), 0);
-  renderer.load_frame(view(img));
+    boost::gil::rgb8_image_t out_image(renderer.width(), renderer.height());
+    boost::gil::copy_and_convert_pixels(view(img), view(out_image));
 
-  boost::gil::rgb8_image_t out_image(renderer.width(), renderer.height());
-  boost::gil::copy_and_convert_pixels(view(img), view(out_image));
+    auto path = boost::dll::program_location().remove_filename() / "rect.bmp";
 
-  auto path =
-    boost::dll::program_location().remove_filename() / "out_image.bmp";
-
-  boost::gil::write_view(path.string(), view(out_image), boost::gil::bmp_tag());
+    boost::gil::write_view(
+      path.string(), view(out_image), boost::gil::bmp_tag());
+  }
 }
