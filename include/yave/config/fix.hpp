b72a544e6fbf14e50bd5d5060060b302c476e903
mocabe <hisa.mocabe@gmail.com>
2020-04-29 14:26:42 +0900

    [config] Add fix_lambda()

---
+++ include/yave/config/fix.hpp
@@ -0,0 +1,35 @@
+//
+// Copyright (c) 2019 mocabe (https://github.com/mocabe)
+// Distributed under LGPLv3 License. See LICENSE for more details.
+//
+
+#pragma once
+
+namespace yave {
+
+  /// helper class to fix lambda
+  template <class F>
+  struct fix_lambda : private F
+  {
+    template <class G>
+    constexpr fix_lambda(G&& g)
+      : F {std::forward<G>(g)}
+    {
+    }
+
+    template <typename... Args>
+    constexpr decltype(auto) operator()(Args&&... args) const
+    {
+      return F::operator()(*this, std::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    constexpr decltype(auto) operator()(Args&&... args)
+    {
+      return F::operator()(*this, std::forward<Args>(args)...);
+    }
+  };
+
+  template <class F>
+  fix_lambda(F &&)->fix_lambda<std::decay_t<F>>;
+}
