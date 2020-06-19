e611e0d571c4263a7b20e926749400db39079ce0
mocabe <hisa.mocabe@gmail.com>
2020-06-20 04:04:37 +0900

    [editor] Add more stuff to editor_data

--- include/yave/editor/editor_data.hpp
+++ include/yave/editor/editor_data.hpp
@@ -5,6 +5,10 @@
 
 #pragma once
 
+#include <yave/module/module.hpp>
+#include <yave/module/module_loader.hpp>
+#include <yave/node/core/node_declaration_store.hpp>
+#include <yave/node/core/node_definition_store.hpp>
 #include <yave/node/core/structured_node_graph.hpp>
 #include <yave/lib/scene/scene_config.hpp>
 
@@ -16,13 +20,22 @@ namespace yave::editor {
   /// editor data
   struct editor_data
   {
-    /// data name
+  public: /* main */
+    /// name
     std::string name;
     /// file path
     std::filesystem::path path;
+
+  public: /* node */
+    /// scene config
+    yave::scene_config scene_config;
+    /// module loader
+    std::unique_ptr<yave::module_loader> module_loader;
+    /// node decl list
+    node_declaration_store node_decls;
+    /// node def list
+    node_definition_store node_defs;
     /// node graph
     structured_node_graph node_graph;
-    /// scene config
-    scene_config scene_config;
   };
-}
+} // namespace yave::editor
