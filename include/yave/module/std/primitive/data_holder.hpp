a44b3702191442d920be6610398fd64314e569c5
mocabe <hisa.mocabe@gmail.com>
2020-07-11 02:28:50 +0900

    [module][std/prim] Add customization point for data type holder property

--- include/yave/module/std/primitive/data_holder.hpp
+++ include/yave/module/std/primitive/data_holder.hpp
@@ -14,17 +14,22 @@ namespace yave {
 
     struct data_type_holder_object_value
     {
-      data_type_holder_object_value(object_ptr<Object> d, object_ptr<Object> c)
-        : m_data {std::move(d)}
-        , m_ctor {std::move(c)}
+      template <class Property, class Ctor>
+      data_type_holder_object_value(
+        const object_ptr<Property>& p,
+        const object_ptr<Ctor>& c)
+        : m_property {p}
+        , m_ctor {c}
+        , m_data {p->initial_value()}
       {
       }
 
       data_type_holder_object_value(const data_type_holder_object_value& other)
       {
-        // only clone data (to avoid sharing)
+        m_property = other.m_property;
+        m_ctor     = other.m_ctor;
+        // clone data (to avoid sharing)
         m_data = other.m_data.clone();
-        m_ctor = other.m_ctor;
       }
 
       [[nodiscard]] auto get_data_constructor() -> object_ptr<Object>
@@ -42,9 +47,15 @@ namespace yave {
         return m_ctor;
       }
 
+      [[nodiscard]] auto property()
+      {
+        return m_property;
+      }
+
     private:
-      object_ptr<Object> m_data; // data
-      object_ptr<Object> m_ctor; // constructor function referring data
+      object_ptr<Object> m_property; // property of data
+      object_ptr<Object> m_ctor;     // constructor function referring data
+      object_ptr<Object> m_data;     // current data
     };
 
     template <class T>
@@ -52,20 +63,64 @@ namespace yave {
     {
       auto code() const -> typename DataTypeCtor::return_type
       {
-        return DataTypeCtor::template eval_arg<0>();
+        return DataTypeCtor::template arg<0>();
       }
     };
+
+    struct data_type_holder_property_value
+    {
+      /// initial value of data
+      object_ptr<const Object> m_init_value;
+
+      template <class T>
+      data_type_holder_property_value(object_ptr<T> init_value)
+        : m_init_value {std::move(init_value)}
+      {
+      }
+
+      data_type_holder_property_value(
+        const data_type_holder_property_value& other) = default;
+
+      auto initial_value() const
+      {
+        return m_init_value.clone();
+      }
+    };
+
+    // default data type property
+    using DataTypeHolderProperty = Box<data_type_holder_property_value>;
+
   } // namespace detail
 
+  /// Customize point for data type holder property object
+  template <class T>
+  struct data_type_holder_traits
+  {
+    template <class... Args>
+    static auto make_property_object(Args&&... args)
+    {
+      // create default property
+      return make_object<detail::DataTypeHolderProperty>(
+        make_object<T>(std::forward<Args>(args)...));
+    }
+  };
+
+  /// Data type holder
   using DataTypeHolder = Box<detail::data_type_holder_object_value>;
 
+  /// Create new data type holder
   template <class T, class... Args>
   [[nodiscard]] auto make_data_type_holder(Args&&... args)
   {
     return make_object<DataTypeHolder>(
-      make_object<T>(std::forward<Args>(args)...),
+      data_type_holder_traits<T>::make_property_object(
+        std::forward<Args>(args)...),
       make_object<detail::DataTypeCtor<T>>());
   }
 } // namespace yave
 
 YAVE_DECL_TYPE(yave::DataTypeHolder, "8cfff49e-b86b-4cca-8d9d-34800003ff3b");
+
+YAVE_DECL_TYPE(
+  yave::detail::DataTypeHolderProperty,
+  "db9db89f-9588-4332-8946-790f39ed282a");
