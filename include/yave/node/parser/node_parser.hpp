c8facc78e952da9613c436e23e53165374345d2e
mocabe <hisa.mocabe@gmail.com>
2020-09-21 22:59:31 +0900

    [node][node_parser] introduce new parser result types

--- include/yave/node/parser/node_parser.hpp
+++ include/yave/node/parser/node_parser.hpp
@@ -5,6 +5,7 @@
 
 #pragma once
 
+#include <yave/node/parser/parse_result.hpp>
 #include <yave/node/core/structured_node_graph.hpp>
 #include <yave/support/error.hpp>
 
@@ -12,19 +13,57 @@
 
 namespace yave {
 
-  /// parse result
-  struct node_parser_result
-  {
-    /// Parsed node graph
-    std::optional<structured_node_graph> node_graph = std::nullopt;
-    /// Error list
-    error_list errors = {};
+  // fwd
+  class node_parser;
 
-    /// success check by bool conversion
-    operator bool() const
-    {
-      return node_graph.has_value();
-    }
+  /// parse result
+  class node_parser_result
+  {
+    class impl;
+    std::unique_ptr<impl> m_pimpl;
+
+  public:
+    /// defualt init
+    node_parser_result();
+    /// dtor
+    ~node_parser_result() noexcept;
+    /// move
+    node_parser_result(node_parser_result&&) noexcept;
+    /// move assign
+    node_parser_result& operator=(node_parser_result&&) noexcept;
+
+  public:
+    /// move out node graph
+    auto take_node_graph() -> std::optional<structured_node_graph>;
+
+    /// has error?
+    bool has_error() const;
+
+    /// filtering results
+    auto get_errors() const -> std::vector<parse_result>;
+    auto get_warnings() const -> std::vector<parse_result>;
+    auto get_infos() const -> std::vector<parse_result>;
+
+    /// get reuslts associated to node.
+    /// \param ng reference node graph
+    /// \param n  target node
+    auto get_results(const structured_node_graph& ng, const node_handle& n)
+      const -> std::vector<parse_result>;
+
+    /// get reuslt associated to socket.
+    /// \param ng referenec node graph
+    /// \param s target socket
+    auto get_results(const structured_node_graph& ng, const socket_handle& s)
+      const -> std::vector<parse_result>;
+
+  private:
+    /// parser uses private functions to add data.
+    friend class node_parser;
+
+    /// add result
+    void add_result(parse_result r);
+    /// set node graph
+    void set_node_graph(structured_node_graph&& ng);
   };
 
   /// Parse node graph and generate data for compiler stage.
@@ -38,11 +77,14 @@ namespace yave {
 
     /// Parse node graph
     /// \param ng node graph to parse
-    /// \param out output socket of the node graph. must be output socket of
-    /// node group.
+    /// \param output_socket output socket of the node graph. must be output
+    /// socket of node group.
+    /// \param visible_group node group which is visible from user. parser will
+    /// provide extra informaton for nodes in the group.
     [[nodiscard]] auto parse(
       structured_node_graph&& ng,
-      const socket_handle& out) -> node_parser_result;
+      const socket_handle& output_socket,
+      const node_handle& visible_group = {}) -> node_parser_result;
 
   private:
     class impl;
--- src/yave/node/parser/parse_result.cpp
+++ src/yave/node/parser/parse_result.cpp
@@ -3,12 +3,12 @@
 // Distributed under LGPLv3 License. See LICENSE for more details.
 //
 
-#include <yave/node/parser/errors.hpp>
+#include <yave/node/parser/parse_result.hpp>
 #include <fmt/format.h>
 
 namespace yave {
 
-  std::string parse_error::missing_input::message() const
+  std::string parse_results::missing_input::message() const
   {
     return fmt::format(
       "Missing input connections: n={}, s={}",
@@ -16,7 +16,7 @@ namespace yave {
       to_string(m_socket_id));
   }
 
-  std::string parse_error::missing_output::message() const
+  std::string parse_results::missing_output::message() const
   {
     return fmt::format(
       "Missing output connections: n={}, s={}",
@@ -24,7 +24,7 @@ namespace yave {
       to_string(m_socket_id));
   }
 
-  std::string parse_error::unexpected_error::message() const
+  std::string parse_results::unexpected_error::message() const
   {
     return fmt::format("Unexpected parse error occured: msg={}", m_msg);
   }
