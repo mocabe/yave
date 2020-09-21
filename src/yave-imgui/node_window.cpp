86c83b97a6c1ceff58bdd2a62b046d60abdfc720
mocabe <hisa.mocabe@gmail.com>
2020-09-21 23:08:53 +0900

    [yave-imgui] Minimal supoprt for new parser results

--- src/yave-imgui/node_window.cpp
+++ src/yave-imgui/node_window.cpp
@@ -178,8 +178,8 @@ namespace yave::editor::imgui {
       auto data_lck = dctx.lock();
       auto& data    = data_lck.template get_data<editor_data>();
 
-      for (auto&& e : data.compiler.parse_errors())
-        ImGui::TextColored({255, 0, 0, 255}, "%s", e.message().c_str());
+      for (auto&& e : data.compiler.parse_result().get_errors())
+        ImGui::TextColored({255, 0, 0, 255}, "%s", message(e).c_str());
 
       for (auto&& e : data.compiler.compile_errors())
         ImGui::TextColored({255, 0, 0, 255}, "%s", e.message().c_str());
