eb4fd29f60020f8c1efd40a25bc162daba099f93
mocabe <hisa.mocabe@gmail.com>
2020-09-25 05:43:36 +0900

    [vcpkg] Migrate blend2d to vcpkg

--- external/cmake/blend2d.cmake
+++ external/cmake/blend2d.cmake
@@ -1,4 +1,8 @@
 # blend2d
-set(BLEND2D_NO_JIT ON)
-set(BLEND2D_STATIC ON)
-init_add_submodule(blend2d)
+find_library(blend2d_LIBRARIES blend2d REQUIRED)
+find_path(blend2d_INCLUDES blend2d.h REQUIRED)
+
+add_library(blend2d INTERFACE)
+target_link_libraries(blend2d INTERFACE ${blend2d_LIBRARIES})
+target_include_directories(blend2d INTERFACE ${blend2d_INCLUDES})
+target_compile_options(blend2d INTERFACE -DBL_STATIC)
