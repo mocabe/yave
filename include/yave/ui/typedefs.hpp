ab976e824068b54fef282b568ce19d96c70f6449
hisa.mocabe <hisa.mocabe@gmail.com>
2021-02-11 20:10:56 +0900

    [ui] Add basic typedefs

---
+++ include/yave/ui/typedefs.hpp
@@ -0,0 +1,24 @@
+//
+// Copyright (c) 2019 mocabe (https://github.com/mocabe)
+// Distributed under LGPLv3 License. See LICENSE for more details.
+//
+
+#pragma once
+
+#include <cstdint>
+
+namespace yave::ui {
+
+  // sized literal types
+  using i8  = int8_t;
+  using u8  = uint8_t;
+  using i16 = int16_t;
+  using u16 = uint16_t;
+  using i32 = int32_t;
+  using u32 = uint32_t;
+  using i64 = int64_t;
+  using u64 = uint64_t;
+  using f32 = float;
+  using f64 = double;
+
+}
