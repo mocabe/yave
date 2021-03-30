ef387ebac6c06343b52b1388513a7e3d2210cb17
mocabe <hisa.mocabe@gmail.com>
2021-03-30 22:44:01 +0900

    [vulkan] Add VulkanMemoryAllocator

---
+++ src/yave/lib/vulkan/vk_mem_alloc.cpp
@@ -0,0 +1,7 @@
+//
+// Copyright (c) 2019 mocabe (https://github.com/mocabe)
+// Distributed under LGPLv3 License. See LICENSE for more details.
+//
+
+#define VMA_IMPLEMENTATION
+#include <vk_mem_alloc.h>
